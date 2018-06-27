67f3463499f12be0ae806a77b511effa0d907e17
Chris Warburton <chriswarbo@gmail.com>
2018-06-27 13:34:24 +0100

    Simplify warbo-utilities definition

--- packages/warbo-utilities.nix
+++ packages/warbo-utilities.nix
@@ -1,16 +1,16 @@
-{ pkgHasBinary, repoSource ? http://chriswarbo.net/git, self, withLatestGit,
-  forceLatest ? false }:
+# Override forceLatest to get the latest version
+{ defaultRepo, forceLatest ? false, latestGit, pkgHasBinary,
+  repoSource ? defaultRepo }:
 
 with {
-  pkg = withLatestGit {
-    url      = "${repoSource}/warbo-utilities.git";
-    srcToPkg = src: import "${src}" { nixPkgs = self; };
-    stable   = {
+  pkg = import (latestGit {
+    url    = "${repoSource}/warbo-utilities.git";
+    stable = {
       rev        = "e806837";
       sha256     = "05g1kqaqymnnnfxx516jilz5a9salhv5ggcppfi0bbmmd7i1jy8g";
       unsafeSkip = forceLatest;
     };
-  };
+  });
 };
 
 pkgHasBinary "jo" pkg
