a8a6fe68885c401a00b335341f5f03d89216a8ae
Chris Warburton <chriswarbo@gmail.com>
2018-07-09 06:38:42 +0100

    Split skulpture into qt4 version (qt5 to come later)

--- packages/skulpture.nix
+++ packages/skulpture.nix
@@ -1,48 +1,51 @@
-{ cmake, fail, fetchurl, findutils, kdelibs4 ? nixpkgs1709.kdelibs4,
-  nixpkgs1709, stdenv, writeScript }:
+{ cmake, fail, fetchFromGitHub, fetchurl, findutils,
+  kdelibs4 ? nixpkgs1709.kdelibs4, nixpkgs1709, qt4, qt5, stdenv, writeScript }:
 
-stdenv.mkDerivation {
-  name    = "skulpture";
-  version = "0.2.4";
-  src     = fetchurl {
-    url    = "http://skulpture.maxiom.de/releases/skulpture-0.2.4.tar.gz";
-    sha256 = "1s27xqd32ck09r1nnjp1pyxwi0js7a7rg2ppkvq2mk78nfcl6sk0";
-  };
+with builtins;
+{
+  qt4 = stdenv.mkDerivation {
+    name    = "skulpture-q4";
+    version = "0.2.4";
+    src     = fetchurl {
+      url    = "http://skulpture.maxiom.de/releases/skulpture-0.2.4.tar.gz";
+      sha256 = "1s27xqd32ck09r1nnjp1pyxwi0js7a7rg2ppkvq2mk78nfcl6sk0";
+    };
 
-  toMove = writeScript "toMove.tsv" ''
-    skulpture.themerc	share/kde4/apps/kstyle/themes
-    skulpture.desktop	share/kde4/apps/kwin
-    skulpture.png	share/kde4/apps/skulpture/pics
-    skulptureui.rc	share/kde4/apps/skulpture
-    libskulpture.so	lib/qt4/plugins/styles
-  '';
+    toMove = writeScript "toMove.tsv" ''
+      skulpture.themerc	share/kde4/apps/kstyle/themes
+      skulpture.desktop	share/kde4/apps/kwin
+      skulpture.png	share/kde4/apps/skulpture/pics
+      skulptureui.rc	share/kde4/apps/skulpture
+      libskulpture.so	lib/qt4/plugins/styles
+    '';
 
-  buildInputs = [ cmake fail kdelibs4 findutils ];
+    buildInputs  = [ cmake fail kdelibs4 findutils qt4 ];
 
-  installPhase = ''
-    cd ..
+    installPhase = ''
+      cd ..
 
-    mkdir -p "$out/share/doc"
-    for DOC in README AUTHORS COPYING NEWS NOTES BUGS
-    do
-      cp -v "$DOC" "$out/share/doc/"
-    done
-
-    mkdir -p "$out/share/kde4/apps/"
-    cp -rv color-schemes "$out/share/kde4/apps/"
-
-    while read -r PAIR
-    do
-      F=$(echo "$PAIR" | cut -f1)
-      D=$(echo "$PAIR" | cut -f2)
-      mkdir -p "$out/$D"
-      FOUND=0
-      while read -r GOT
+      mkdir -p "$out/share/doc"
+      for DOC in README AUTHORS COPYING NEWS NOTES BUGS
       do
-        cp -rv "$GOT" "$out/$D/"
-        FOUND=1
-      done < <(find . -name "$F")
-      [[ "$FOUND" -eq 1 ]] || fail "Didn't find '$F', aborting"
-    done < "$toMove"
-  '';
+        cp -v "$DOC" "$out/share/doc/"
+      done
+
+      mkdir -p "$out/share/kde4/apps/"
+      cp -rv color-schemes "$out/share/kde4/apps/"
+
+      while read -r PAIR
+      do
+        F=$(echo "$PAIR" | cut -f1)
+        D=$(echo "$PAIR" | cut -f2)
+        mkdir -p "$out/$D"
+        FOUND=0
+        while read -r GOT
+        do
+          cp -rv "$GOT" "$out/$D/"
+          FOUND=1
+        done < <(find . -name "$F")
+        [[ "$FOUND" -eq 1 ]] || fail "Didn't find '$F', aborting"
+      done < "$toMove"
+    '';
+  };
 }
