b9343302ed23a50fa9d7218cbfd8b55fe3a55288
Chris Warburton <chriswarbo@gmail.com>
2018-07-17 00:46:52 +0100

    Bump nix-helpers to fix e.g. 'override' attributes

--- helpers.nix
+++ helpers.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? import <nixpkgs> { config = {}; overlays = []; } }:
 import (nixpkgs.fetchgit {
   url    = http://chriswarbo.net/git/nix-helpers.git;
-  rev    = "96a2fa3";
-  sha256 = "0j5xxgjbyjsj9ayj3q7b95s7gzmmahwlj27nvbmdjyrxk3dn7gxz";
+  rev    = "03a263f";
+  sha256 = "0mw6kf902fkhp7rpx8rfqjr6b7nzky5wyayabmqyr3gj2lngfxn1";
 })
