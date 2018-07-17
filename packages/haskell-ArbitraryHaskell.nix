2b80cb52b2310032c4c702522dd94fe1af6c3d4b
Chris Warburton <chriswarbo@gmail.com>
2018-07-17 03:29:40 +0100

    Pull ArbitraryHaskell out of its wrappers

--- packages/haskell-ArbitraryHaskell.nix
+++ packages/haskell-ArbitraryHaskell.nix
@@ -1,7 +1,10 @@
 { haskellPackages, haskellRelease, unpack }:
 
-haskellRelease {
-  name        = "ArbitraryHaskell";
-  dir         = unpack haskellPackages.ArbitraryHaskell.src;
-  hackageSets = { nixpkgs1709 = [ "ghc7103" ]; };
-}
+with {
+  all = haskellRelease {
+    name        = "ArbitraryHaskell";
+    dir         = unpack haskellPackages.ArbitraryHaskell.src;
+    hackageSets = { nixpkgs1709 = [ "ghc7103" ]; };
+  };
+};
+all.hackageDeps.nixpkgs1709.ghc7103
