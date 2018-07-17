d18dc3c983c1f4a524784475ad74100216ec3485
Chris Warburton <chriswarbo@gmail.com>
2018-07-17 12:20:02 +0100

    Bump nix-helpers to get cycle-breaking runCabal2nix

--- helpers.nix
+++ helpers.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? import <nixpkgs> { config = {}; overlays = []; } }:
 nixpkgs.fetchgit {
   url    = http://chriswarbo.net/git/nix-helpers.git;
-  rev    = "4f50621";
-  sha256 = "1c90yk10jrwx7g475i06jwc8xq92008x82zlldkbli8hb9gbl45n";
+  rev    = "3f34310";
+  sha256 = "170fmx8yk21x8imdg4qf3g6np42wqrzq3i1rfc6ssqrsbqm0wbic";
 }
