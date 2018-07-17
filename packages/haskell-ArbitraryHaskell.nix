b231feb627b3c87fce65cc5023236e02aee9c607
Chris Warburton <chriswarbo@gmail.com>
2018-07-17 03:24:38 +0100

    Use haskellRelease to pick deps for ArbitraryHaskell

--- packages/haskell-ArbitraryHaskell.nix
+++ packages/haskell-ArbitraryHaskell.nix
@@ -1,18 +1,7 @@
-{ haskell, lib, runCabal2nix }:
+{ haskellPackages, haskellRelease, unpack }:
 
-with {
-  hsPkgs = haskell.packages.ghc7103.override (old: {
-    overrides = lib.composeExtensions
-      (old.overrides or (_: _: {}))
-      (self: super: {
-        haskell-src-exts = self.callPackage (runCabal2nix {
-          url = "cabal://haskell-src-exts-1.17.1";
-        }) {};
-        semigroups = self.callPackage (runCabal2nix {
-          url = "cabal://semigroups-0.14";
-        }) {};
-      });
-  });
-};
-
-hsPkgs.ArbitraryHaskell
+haskellRelease {
+  name        = "ArbitraryHaskell";
+  dir         = unpack haskellPackages.ArbitraryHaskell.src;
+  hackageSets = { nixpkgs1709 = [ "ghc7103" ]; };
+}
