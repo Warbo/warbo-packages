247115566d29baf5e08f3acb1f64b42e366131dc
Chris Warburton <chriswarbo@gmail.com>
2018-07-26 23:56:38 +0100

    Qualify names

--- haskell/panhandle.nix
+++ haskell/panhandle.nix
@@ -5,8 +5,8 @@ with {
   # We must use a git checkout due to a bug in panhandle.cabal: the test suite
   # doesn't put its internal modules in 'other-modules', so they don't end up in
   # the sdist tarball. This is fixed in 0.3 but we want 0.2 for dependencies.
-  panhandleSrc = trace "FIXME: Backport panhandle 'other-modules' patch to 2.x and push to Hackage" latestGit {
-    url    = "${repoSource}/panhandle.git";
+  panhandleSrc = trace "FIXME: Backport panhandle 'other-modules' patch to 2.x and push to Hackage" self.latestGit {
+    url    = "${self.repoSource or self.defaultRepo}/panhandle.git";
     stable = {
       rev        = "83eb3a7";
       sha256     = "0sc43023ww4kakxyhzb26fv15b633a5n7aiqayrida87686w3icn";
