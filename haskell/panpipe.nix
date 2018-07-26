7a6dbe2fd1cbbe45788d0e398a0f11547d19449a
Chris Warburton <chriswarbo@gmail.com>
2018-07-27 00:23:17 +0100

    Use panpipe 0.2

--- haskell/panpipe.nix
+++ haskell/panpipe.nix
@@ -1,10 +1,11 @@
 self: super: helf: huper:
 
-huper.panpipe or (helf.callPackage (self.haskellGit {
-  url    = "${self.repoSource or self.defaultRepo}/panpipe.git";
-  stable = {
-    rev        = "7b8aba6";
-    sha256     = "0m5f9j1474f9w8mi2n93df9yzfycavagj1b1kkxcxb6c7j4dxidl";
-    unsafeSkip = false;
-  };
-}) {})
+helf.callPackage (self.runCabal2nix {
+  url = self.unpack (self.fetchurl {
+    url    = "https://hackage.haskell.org/package/panpipe-0.2.0.0/panpipe-0.2.0.0.tar.gz";
+    sha256 = "0kdkw7y6hvdv3lz4fhq4x0f7y397753dw5mjp4gw03qnrz3nchxp";
+  });
+}) {
+  mkDerivation = args: helf.mkDerivation
+    (removeAttrs args [ "benchmarkHaskellDepends" ]);
+}
