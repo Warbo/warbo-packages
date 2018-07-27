13012b33ec5075815bdf90375b51f6fdeaa47266
Chris Warburton <chriswarbo@gmail.com>
2018-07-27 01:43:57 +0100

    Bump helpers

--- helpers.nix
+++ helpers.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? import <nixpkgs> { config = {}; overlays = []; } }:
 nixpkgs.fetchgit {
   url    = http://chriswarbo.net/git/nix-helpers.git;
-  rev    = "510099c";
-  sha256 = "04d6s7a8m1rjkj1kjz4myx3x80dh5jrrjkwqvsydmq41vafg7lhx";
+  rev    = "640102c";
+  sha256 = "1v8w012v7j2xg30dlh1i4y933v8ykiq3cc3xw4v92qv7pwfai4zf";
 }
