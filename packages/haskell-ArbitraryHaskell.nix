c9ca32a9e86407fd34a52bd0eb9cb7781e11de25
Chris Warburton <chriswarbo@gmail.com>
2018-07-27 01:33:14 +0100

    Disable ArbitraryHaskell for now

--- packages/haskell-ArbitraryHaskell.nix
+++ packages/haskell-ArbitraryHaskell.nix
@@ -1,10 +1,12 @@
-{ haskellPackages, haskellRelease, unpack }:
+{ nothing, haskellPackages, haskellRelease, unpack }:
 
 with {
-  all = haskellRelease {
-    name        = "ArbitraryHaskell";
-    dir         = unpack haskellPackages.ArbitraryHaskell.src;
-    hackageSets = { nixpkgs1709 = [ "ghc7103" ]; };
+  all = haskellRelease rec {
+    name         = "ArbitraryHaskell";
+    dir          = unpack haskellPackages.ArbitraryHaskell.src;
+    extraSources = { ArbitraryHaskell = dir; };
+    hackageSets  = { nixpkgs1709 = [ "ghc7103" ]; };
   };
 };
-all.hackageDeps.nixpkgs1709.ghc7103
+#all.hackageDeps.nixpkgs1709.ghc7103
+builtins.trace "FIXME: No ArbitraryHaskell package" nothing
