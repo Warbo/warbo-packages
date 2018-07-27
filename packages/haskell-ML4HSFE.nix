b0609def467bdc9026b1e41d3ca19bd753c8118b
Chris Warburton <chriswarbo@gmail.com>
2018-07-27 14:20:16 +0100

    Force quickspec 1

--- packages/haskell-ML4HSFE.nix
+++ packages/haskell-ML4HSFE.nix
@@ -3,4 +3,8 @@
 (haskellOverride {
   haskellPackages = haskell.packages.ghc7103;
   filepath        = true;
+  extra           = helf: huper: {
+    # Force version 1 of quickspec
+    quickspec = helf.callHackage "quickspec" "0.9.6" {};
+  };
 }).ML4HSFE
