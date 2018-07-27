e62daa400f6705941197fd9b5ac886665e2d17b3
Chris Warburton <chriswarbo@gmail.com>
2018-07-27 14:30:42 +0100

    Fix typo

--- packages/haskell-ML4HSFE.nix
+++ packages/haskell-ML4HSFE.nix
@@ -5,7 +5,7 @@
   filepath        = true;
   extra           = helf: huper: {
     # Version 2.10+ causes an "ambiguous 'total'" error in quickspec
-    QuickCheck = helf.callHackage "QuickCHeck" "2.9.2" {};
+    QuickCheck = helf.callHackage "QuickCheck" "2.9.2" {};
 
     # Force version 1 of quickspec, since nixpkgs may be using 2.x
     quickspec = helf.callHackage "quickspec" "0.9.6" {};
