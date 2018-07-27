c2a184a49059ac54565dcb6941543df8195b2ab9
Chris Warburton <chriswarbo@gmail.com>
2018-07-27 13:06:08 +0100

    Patch system-filepath for AstPlugin

--- packages/haskell-AstPlugin.nix
+++ packages/haskell-AstPlugin.nix
@@ -1,3 +1,6 @@
-{ haskell }:
+{ haskell, haskellOverride }:
 
-haskell.packages.ghc7103.AstPlugin
+(haskellOverride {
+  haskellPackages = haskell.packages.ghc7103;
+  filepath        = true;
+}).AstPlugin
