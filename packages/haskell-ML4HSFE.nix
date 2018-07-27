f19780729196c143cde54f3c02f79771030dbaf2
Chris Warburton <chriswarbo@gmail.com>
2018-07-27 14:26:34 +0100

    Force QuickCheck version in ML4HSFE

--- packages/haskell-ML4HSFE.nix
+++ packages/haskell-ML4HSFE.nix
@@ -4,7 +4,10 @@
   haskellPackages = haskell.packages.ghc7103;
   filepath        = true;
   extra           = helf: huper: {
-    # Force version 1 of quickspec
+    # Version 2.10+ causes an "ambiguous 'total'" error in quickspec
+    QuickCheck = helf.callHackage "QuickCHeck" "2.9.2" {};
+
+    # Force version 1 of quickspec, since nixpkgs may be using 2.x
     quickspec = helf.callHackage "quickspec" "0.9.6" {};
   };
 }).ML4HSFE
