e047906b583d00d530506ac9f72c4a55f02e2814
Chris Warburton <chriswarbo@gmail.com>
2018-07-28 21:19:54 +0100

    Force GHC 7 for panhandle

--- packages/panhandle.nix
+++ packages/panhandle.nix
@@ -1,6 +1,6 @@
 { haskellOverride, nixpkgs1609 }:
 
 (haskellOverride {
-  haskellPackages = nixpkgs1609.haskellPackages;
+  haskellPackages = nixpkgs1609.haskell.packages.ghc7103;
   panPkgs         = true;
 }).panhandle
