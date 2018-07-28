b6fc0fca04847b46869a6a445c22af373dbea80e
Chris Warburton <chriswarbo@gmail.com>
2018-07-28 21:13:59 +0100

    Patch system-filepath for mlspec

--- packages/haskell-mlspec.nix
+++ packages/haskell-mlspec.nix
@@ -3,7 +3,8 @@
 with lib;
 (haskellOverride {
   haskellPackages = haskell.packages.ghc7103;
-  extra = [
+  filepath        = true;
+  extra           = [
     (self: super: mapAttrs (n: v: self.callHackage n v {}) {
       # Force version 1 of quickspec, since nixpkgs may be using 2.x
       quickspec = "0.9.6";
