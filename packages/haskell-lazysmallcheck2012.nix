640294ada90f663e5a5d8a7a24e1eaa4eca84f6f
Chris Warburton <chriswarbo@gmail.com>
2018-07-28 13:12:14 +0100

    Force SYB for lazysmallcheck2012

--- packages/haskell-lazysmallcheck2012.nix
+++ packages/haskell-lazysmallcheck2012.nix
@@ -1,3 +1,10 @@
-{ haskell }:
+{ haskell, haskellOverride }:
 
-haskell.packages.ghc7103.lazysmallcheck2012
+(haskellOverride {
+  haskellPackages = haskell.packages.ghc7103;
+  extra           = [
+    (self: super: {
+      syb = self.callHackage "syb" "0.6" {};
+    })
+  ];
+}).lazysmallcheck2012
