4fc21ae0ab02382cf5bc4e841c9adb820b06e860
Chris Warburton <chriswarbo@gmail.com>
2018-07-29 00:39:31 +0100

    Force haskell-src-exts version

--- packages/haskell-runtime-arbitrary-tests.nix
+++ packages/haskell-runtime-arbitrary-tests.nix
@@ -1,14 +1,27 @@
 { haskell, haskellOverride, lib }:
 
 with lib;
-(haskellOverride {
-  haskellPackages = haskell.packages.ghc7103;
-  extra           = [
-    (self: super: mapAttrs (n: v: self.callHackage n v {}) {
-      haskell-src-exts = "1.16.0.1";
+with {
+  haskellPackages = haskellOverride {
+    haskellPackages = haskell.packages.ghc7103;
+    extra           = [
+      (self: super: mapAttrs (n: v: self.callHackage n v {}) {
+        haskell-src-exts = "1.16.0.1";
 
-      # Nixpkgs version is missing semigroups dependency
-      transformers-compat = "0.5.1.4";
-    })
-  ];
-}).runtime-arbitrary-tests
+        # Nixpkgs version is missing semigroups dependency
+        transformers-compat = "0.5.1.4";
+      })
+      (self: super: {
+        hindent  = super.hindent.override {
+          inherit (self) haskell-src-exts;
+        };
+
+        nix-eval = super.nix-eval.override {
+          inherit (self) hindent;
+        };
+      })
+    ];
+  };
+};
+with builtins; trace (toJSON haskellPackages.runtime-arbitrary-tests.buildInputs)
+haskellPackages.runtime-arbitrary-tests
