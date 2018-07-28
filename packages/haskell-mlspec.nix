70984737d099ef2e891a67f9443e80a9f5a90f3d
Chris Warburton <chriswarbo@gmail.com>
2018-07-28 13:15:51 +0100

    Force quickspec 1 in mlspec

--- packages/haskell-mlspec.nix
+++ packages/haskell-mlspec.nix
@@ -1,3 +1,12 @@
-{ haskell }:
+{ haskell, haskellOverride, lib }:
 
-haskell.packages.ghc7103.mlspec
+with lib;
+(haskellOverride {
+  haskellPackages = haskell.packages.ghc7103;
+  extra = [
+    (self: super: mapAttrs (n: v: self.callHackage n v {}) {
+      # Force version 1 of quickspec, since nixpkgs may be using 2.x
+      quickspec = "0.9.6";
+    })
+  ];
+}).mlspec
