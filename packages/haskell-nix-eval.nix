3007bd3da0b0d78bb632d8b118deaecd95b86d6c
Chris Warburton <chriswarbo@gmail.com>
2018-07-28 23:00:53 +0100

    Force haskell-src-exts version

--- packages/haskell-nix-eval.nix
+++ packages/haskell-nix-eval.nix
@@ -1,11 +1,15 @@
-{ haskell, haskellOverride, lib }:
+{ die, haskell, haskellOverride, lib }:
 
 with lib;
-(haskellOverride {
+with {
+  haskellPackages = haskellOverride {
   haskellPackages = haskell.packages.ghc7103;
-  extra           = [
-    (self: super: mapAttrs (n: v: self.callHackage n v {}) {
-      haskell-src-exts = "1.16.0.1";
-    })
-  ];
-}).nix-eval
+    extra           = [
+      (self: super: mapAttrs (n: v: self.callHackage n v {}) {
+        haskell-src-exts = "1.16.0.1";
+        hindent          = "5.2.5";
+      })
+    ];
+  };
+};
+haskellPackages.nix-eval
