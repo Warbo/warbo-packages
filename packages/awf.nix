fc9679a54f9e928f44f8795454093d992c813e0e
Chris Warburton <chriswarbo@gmail.com>
2018-07-28 20:52:12 +0100

    Fix AWF
    In particular, it looks like using 'pkg' as an env var is problematic. Not sure
    if the 'bins' wrapper is needed; it prevents us using icons, etc.

--- packages/awf.nix
+++ packages/awf.nix
@@ -1,9 +1,9 @@
-{ autoconf, automake, fetchFromGitHub, gcc, gtk2, gtk3, hasBinary, pkgconfig,
-  runCommand, stdenv, stripOverrides, widgetThemes ? {} }:
+{ autoconf, automake, fail, fetchFromGitHub, gcc, gtk2, gtk3, hasBinary,
+  pkgconfig, runCommand, stdenv, stripOverrides, widgetThemes ? {} }:
 
 with builtins;
 with rec {
-  pkg = stdenv.mkDerivation {
+  whole = stdenv.mkDerivation {
     name = "awf";
     src  = fetchFromGitHub {
       owner  = "valr";
@@ -15,21 +15,12 @@ with rec {
                    attrValues (stripOverrides widgetThemes);
     preConfigure = "bash autogen.sh";
   };
-
-  # Only expose binaries, to prevent path conflicts
-  bins = runCommand "awf-binaries" { inherit pkg; } ''
-    mkdir -p "$out/bin"
-    for B in "$pkg/bin"/*
-    do
-    cp -s "$B" "$out/bin/"
-    done
-  '';
 };
 
-{
-  pkg   = bins;
+rec {
+  pkg   = whole;
   tests = {
-    gtk2 = hasBinary bins "awf-gtk2";
-    gtk3 = hasBinary bins "awf-gtk3";
+    gtk2 = hasBinary pkg "awf-gtk2";
+    gtk3 = hasBinary pkg "awf-gtk3";
   };
 }
