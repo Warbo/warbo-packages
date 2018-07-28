cc43ad1455d5edc516b2685380763e9f600ea147
Chris Warburton <chriswarbo@gmail.com>
2018-07-28 20:24:23 +0100

    Formatting

--- packages/racket.nix
+++ packages/racket.nix
@@ -5,8 +5,9 @@ with checkRacket;
 rec {
   pkg   = (if racketWorks
               then super
-              else trace ''WARNING: Taking racket from nixpkgs 16.09, since
-                           it's broken on i686 for newer versions''
-                         nixpkgs1609).racket;
+              else trace ''
+                WARNING: Taking racket from nixpkgs 16.09, since it's broken on
+                i686 for newer versions
+              '' nixpkgs1609).racket;
   tests = checkWhetherBroken;
 }
