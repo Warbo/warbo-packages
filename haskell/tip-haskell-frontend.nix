fcca87783f009539f63cc72d2f493a838720e49f
Chris Warburton <chriswarbo@gmail.com>
2018-07-29 18:06:22 +0100

    Remove tip-haskell-frontend-main indirection

--- haskell/tip-haskell-frontend.nix
+++ haskell/tip-haskell-frontend.nix
@@ -1,3 +1,17 @@
 self: super: helf: huper:
 
-self.haskell.lib.doJailbreak helf.tip-haskell-frontend-main
+with rec {
+  thfSrc = self.runCommand "thf-src" { src = self.tipSrc; } ''
+    D="$src/tip-haskell-frontend"
+    [[ -d "$D" ]] || {
+      echo "Couldn't find '$D'" 1>&2
+      find . 1>&2
+      env 1>&2
+      exit 1
+    }
+    cp -ar "$D" "$out"
+  '';
+
+  thf = helf.callPackage (self.runCabal2nix { url = thfSrc; }) {};
+};
+self.haskell.lib.doJailbreak thf
