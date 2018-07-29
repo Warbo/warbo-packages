4278fe3af086773ea16261cdedefd2798c7bc9ce
Chris Warburton <chriswarbo@gmail.com>
2018-07-29 18:27:34 +0100

    Patch tip-haskell-frontend to use semigroups

--- haskell/tip-haskell-frontend.nix
+++ haskell/tip-haskell-frontend.nix
@@ -1,15 +1,30 @@
 self: super: helf: huper:
 
 with rec {
-  thfSrc = self.runCommand "thf-src" { src = self.tipSrc; } ''
-    D="$src/tip-haskell-frontend"
-    [[ -d "$D" ]] || {
-      echo "Couldn't find '$D'" 1>&2
-      find . 1>&2
-      env 1>&2
-      exit 1
+  thfSrc = self.runCommand "thf-src"
+    {
+      src  = self.tipSrc;
+      expr = "import Options.Applicative";
     }
-    cp -ar "$D" "$out"
+    ''
+      D="$src/tip-haskell-frontend"
+      [[ -d "$D" ]] || {
+        echo "Couldn't find '$D'" 1>&2
+        find . 1>&2
+        env 1>&2
+        exit 1
+      }
+      cp -ar "$D" "$out"
+      chmod +w -R "$out"
+
+      for F in executable/Main.hs src/Tip/Params.hs
+      do
+        sed -e "s/$expr/$expr\nimport Data.Semigroup/g" \
+            -i "$out/$F"
+      done
+
+      sed -e 's/optparse-applicative,/optparse-applicative, semigroups,/g' \
+          -i "$out/tip-haskell-frontend.cabal"
   '';
 
   thf = helf.callPackage (self.runCabal2nix { url = thfSrc; }) {};
