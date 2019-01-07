e9880922c5fa93bd85630b9e80062b8959a5b5f4
Chris Warburton <chriswarbo@gmail.com>
2019-01-07 21:26:23 +0000

    Bump

--- helpers.nix
+++ helpers.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? import <nixpkgs> { config = {}; overlays = []; } }:
 nixpkgs.fetchgit {
   url    = http://chriswarbo.net/git/nix-helpers.git;
-  rev    = "05182d0";
-  sha256 = "1h8jrjrbbab0fymssdwgzl171x6lq817sjd8hzhia7kn5mgm2fib";
+  rev    = "148bd5e";
+  sha256 = "0wywgdmv4gllarayhwf9p41pzrkvgs32shqrycv2yjkwz321w8wl";
 }
