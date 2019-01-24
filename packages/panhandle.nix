54d8a245eaf0787cb93a28ddb4d53e97f7f039a1
Chris Warburton <chriswarbo@gmail.com>
2019-01-24 13:20:01 +0000

    Add test to panhandle

--- packages/panhandle.nix
+++ packages/panhandle.nix
@@ -1,6 +1,12 @@
-{ haskellOverride, nixpkgs1609 }:
+{ hasBinary, haskellOverride, nixpkgs1609 }:
 
-(haskellOverride {
-  haskellPackages = nixpkgs1609.haskell.packages.ghc7103;
-  panPkgs         = true;
-}).panhandle
+rec {
+  pkg = (haskellOverride {
+    haskellPackages = nixpkgs1609.haskell.packages.ghc7103;
+    panPkgs         = true;
+  }).panhandle;
+
+  tests = {
+    haveBinary = hasBinary pkg "panhandle";
+  };
+}
