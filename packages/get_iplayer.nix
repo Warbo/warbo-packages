4b8f47976a7a2619efc7cd10aa7f432d53d26e95
Chris Warburton <chriswarbo@gmail.com>
2019-02-11 22:48:42 +0000

    Bump get_iplayer
    Also added a test which checks whether we're using the latest version (this is
    useful since the BBC keep changing things, so we need to be running the latest
    version)

--- packages/get_iplayer.nix
+++ packages/get_iplayer.nix
@@ -1,18 +1,59 @@
 # Updated get_iplayer
-{ buildEnv, fetchurl, ffmpeg, hasBinary, super, perlPackages, stdenv }:
+{ buildEnv, cacert, fetchurl, ffmpeg, hasBinary, lib, perlPackages, runCommand,
+  stdenv, super, wget, xidel }:
 
-with {
+with lib;
+with rec {
+  # Update this as needed
+  tag    = "v3.18";
+  sha256 = "1mqwwwnspk1cjpb7ccwg6g97rja1nl0c18ggvzfn6qmkdnyx6vp2";
+
+  src    = fetchurl {
+    inherit sha256;
+    url = "https://github.com/get-iplayer/get_iplayer/archive/${tag}.tar.gz";
+  };
+
+  versionTest = runCommand "latest-get_iplayer"
+    {
+      buildInputs = [ wget xidel ];
+      expr        = concatStringsSep "/" [
+        ''//a[contains(text(), "Latest release")]''
+        ".."
+        ".."
+        ''/a[contains(@href, "releases/tag")]''
+        "text()"
+      ];
+
+      SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
+      url = "https://github.com/get-iplayer/get_iplayer/releases";
+    }
+    ''
+      wget -q -O releases.html "$url" || {
+        echo "Couldn't download releases page, skipping test" 1>&2
+        mkdir "$out"
+      }
+
+      LATEST=$(xidel - -q -e "$expr" < releases.html)
+      if [[ "x$LATEST" = "x${tag}" ]]
+      then
+        echo "Latest get_iplayer is '$LATEST', as expected" 1>&2
+        mkdir "$out"
+      else
+        echo "Update get_iplayer! Have '${tag}', but '$LATEST' is out" 1>&2
+        exit 1
+      fi
+    '';
+
   get_iplayer_real = stdenv.lib.overrideDerivation super.get_iplayer
     (oldAttrs : {
-      name = "get_iplayer";
-      src  = fetchurl {
-        url    = https://github.com/get-iplayer/get_iplayer/archive/v3.05.tar.gz;
-        sha256 = "1lk63myf5smm1i9yd9f0ml5cpwj1kqh480y5s1g0mllr4zk8vv5v";
-      };
+      inherit src versionTest;
+      name                  = "get_iplayer-${tag}";
       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
-                                perlPackages.XMLSimple
-                                ffmpeg
-                              ];
+        perlPackages.LWPProtocolHttps
+        perlPackages.XMLSimple
+        ffmpeg
+      ];
     });
 };
 rec {
