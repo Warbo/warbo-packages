9201806558464ca710c5d8064313cb7e1ce658fd
Chris Warburton <chriswarbo@gmail.com>
2019-04-14 17:19:37 +0100

    Use nixpkgs brittany

--- packages/brittany.nix
+++ packages/brittany.nix
@@ -1,20 +1,6 @@
-{ fetchFromGitHub, hasBinary, haskellPackages }:
+{ hasBinary, haskellPackages }:
 
-with {
-  src = fetchFromGitHub {
-    owner  = "lspitzner";
-    repo   = "brittany";
-    rev    = "21ef8b2";
-    sha256 = "0xcf6rp90m34y72fijblcmsdwwwl3g6jbw1g4yc3r5cb8rpks82p";
-  };
-};
 rec {
-  pkg = with haskellPackages;
-        callPackage (haskellSrc2nix {
-                      inherit src;
-                      name = "brittany";
-                    })
-                    {};
-
+  pkg   = haskellPackages.brittany;
   tests = hasBinary pkg "brittany";
 }
