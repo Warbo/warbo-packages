11f2dfd58b8994c19ec784b5cade22c6c90f550d
Chris Warburton <chriswarbo@gmail.com>
2019-04-15 13:54:15 +0100

    Use older qt stuff for rockbox, rather than overriding gcc

--- packages/rockbox_utility.nix
+++ packages/rockbox_utility.nix
@@ -1,5 +1,7 @@
-{ qtbase, super }:
+{ nixpkgs1709, super }:
 
 {
-  pkg = super.rockbox_utility.override (old: { inherit qtbase; });
+  pkg = super.rockbox_utility.override (old: {
+    inherit (nixpkgs1709.qt5) qtbase qmake qttools;
+  });
 }
