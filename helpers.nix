b23cb8ea501418ad3328701b9058c0bda35f5d38
Chris Warburton <chriswarbo@gmail.com>
2019-04-24 14:53:51 +0100

    Bump nix-helpers to get nixpkgs1903

--- helpers.nix
+++ helpers.nix
@@ -2,7 +2,7 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "ea0ea7c";
-    sha256 = "1rchcc10s3spxbbwi9sz4v3cjcgcakq33hrqk9rkznrv8q2v831w";
+    rev    = "09f6312";
+    sha256 = "0yhbqigijfcizygzqi116rpp1bvjn4x8x8bzpk8pkhgq1360vig1";
   };
 }
