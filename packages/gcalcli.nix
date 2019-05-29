97324992bf266ca23fe5b25f2d773890adf66947
Chris Warburton <chriswarbo@gmail.com>
2019-05-29 13:27:19 +0100

    Force gcalcli deps to be from nixpkgs1803

--- packages/gcalcli.nix
+++ packages/gcalcli.nix
@@ -1,38 +1,43 @@
-{ die, fetchurl, hasBinary, lib, pythonPackages, google-api-python-client }:
+{ die, fetchurl, hasBinary, lib, google-api-python-client, nixpkgs1803,
+  uritemplate }:
 
 with builtins;
+with lib;
+with rec {
+  inherit (nixpkgs1803) pythonPackages;
+  got  = pythonPackages.oauth2client.name;
+  want = "1.4.12";
+};
+assert hasSuffix want got || die {
+  inherit got want;
+  error = "oauth2client version should be <= ${want}";
+};
 rec {
-  pkg = lib.makeOverridable
-    ({ google-api-python-client, pythonPackages  }:
-      with {
-        got  = pythonPackages.oauth2client.version;
-        want = "1.4.12";
-      };
-      assert compareVersions got want < 1 || die {
-        inherit got want;
-        error = "oauth2client version should be <= ${want}";
-      };
-      pythonPackages.buildPythonPackage {
-        name = "gcalcli";
-        version = "3.3.2";
+  pkg = pythonPackages.buildPythonPackage {
+    name = "gcalcli";
+    version = "3.3.2";
 
-        src = fetchurl {
-          url = https://pypi.python.org/packages/source/g/gcalcli/gcalcli-3.3.2.tar.gz;
-          sha256 = "0yw60zgh2ski46mxsyncwx4bb6zzrfp5bn91hg0xyvmz71339mkj";
+    src = fetchurl {
+      url = https://pypi.python.org/packages/source/g/gcalcli/gcalcli-3.3.2.tar.gz;
+      sha256 = "0yw60zgh2ski46mxsyncwx4bb6zzrfp5bn91hg0xyvmz71339mkj";
+    };
+
+    propagatedBuildInputs = [
+      pythonPackages.python
+      pythonPackages.gflags
+      pythonPackages.dateutil
+      pythonPackages.vobject
+      pythonPackages.parsedatetime
+      pythonPackages.httplib2
+      pythonPackages.oauth2client
+      (google-api-python-client.override {
+        inherit pythonPackages;
+        uritemplate = uritemplate.override {
+          inherit pythonPackages;
         };
-
-        propagatedBuildInputs = [
-          pythonPackages.python
-          pythonPackages.gflags
-          pythonPackages.dateutil
-          pythonPackages.vobject
-          pythonPackages.parsedatetime
-          pythonPackages.httplib2
-          pythonPackages.oauth2client
-          google-api-python-client
-        ];
       })
-      { inherit google-api-python-client pythonPackages; };
+    ];
+  };
 
   tests = hasBinary pkg "gcalcli";
 }
