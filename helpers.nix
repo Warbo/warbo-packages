d0ecae68709208a925de1d92154f67a56239d41e
Chris Warburton <chriswarbo@gmail.com>
2019-05-30 16:39:21 +0100

    Bump helpers to put wrapped scripts in directories

--- helpers.nix
+++ helpers.nix
@@ -2,7 +2,7 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "dcd2d23";
-    sha256 = "1xq80iq8994za9hhk246z26g8zjgpmd2845d084qqixn0xjgqcjf";
+    rev    = "f834fbb";
+    sha256 = "0pcaml246lkbdd29svsxhld50d4sbd1dqvacs9aj1wgyhhy53537";
   };
 }
