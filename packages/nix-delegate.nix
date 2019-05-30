1df26c704faafcce45208293a6a1637822206dbe
Chris Warburton <chriswarbo@gmail.com>
2019-05-30 10:55:45 +0100

    Force nix-delegate deps but allow override

--- packages/nix-delegate.nix
+++ packages/nix-delegate.nix
@@ -1,16 +1,22 @@
-{ forceLatest ? false, hasBinary, haskellPackages, latestGit }:
+{ hasBinary, latestGit, lib, nixpkgs1803 }:
 
-with {
-  src = latestGit {
+with rec {
+  get = unsafeSkip: latestGit {
     url    = https://github.com/awakesecurity/nix-delegate.git;
     stable = {
-      rev        = "aeb6c4a";
-      sha256     = "0qb83jkb495vgh912sdiqcph7zrppm4rch9j25m5988d9y1ykgja";
-      unsafeSkip = forceLatest;
+      inherit unsafeSkip;
+      rev    = "aeb6c4a";
+      sha256 = "0qb83jkb495vgh912sdiqcph7zrppm4rch9j25m5988d9y1ykgja";
     };
   };
+
+  f = { forceLatest, haskellPackages }:
+    haskellPackages.callPackage (get forceLatest) {};
 };
 rec {
-  pkg   = haskellPackages.callPackage src {};
+  pkg = lib.makeOverridable f {
+    inherit (nixpkgs1803) haskellPackages;
+    forceLatest = false;
+  };
   tests = hasBinary pkg "nix-delegate";
 }
