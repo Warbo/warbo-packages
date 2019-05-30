394f69c3662ab82c9bae06ad60cfbe41d28a7ed8
Chris Warburton <chriswarbo@gmail.com>
2019-05-30 15:18:08 +0100

    Force geniplate to come from older ghc package set

--- packages/haskell-geniplate.nix
+++ packages/haskell-geniplate.nix
@@ -1,3 +1,5 @@
-{ nixpkgs1809 }:
+{ haskellOverride, nixpkgs1709 }:
 
-nixpkgs1809.haskell.packages.ghc7103.geniplate
+(haskellOverride {
+  haskellPackages = nixpkgs1709.haskell.packages.ghc784;
+}).geniplate
