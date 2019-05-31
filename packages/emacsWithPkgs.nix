a5011567af50989056715307e045da72939cd362
Chris Warburton <chriswarbo@gmail.com>
2019-05-31 23:08:25 +0100

    Use latest emacs we can find

--- packages/emacsWithPkgs.nix
+++ packages/emacsWithPkgs.nix
@@ -2,12 +2,15 @@
 
 with builtins;
 with rec {
+  # We want the latest Emacs version, but it gets a little messy:
+  # If there is no emacs25 package, use the emacs package
   version = if emacs25 == null
-               then trace (toJSON {
-                      warning      = "No 'emacs25' provided, so using 'emacs'";
-                      emacsVersion = super.emacs.version;
-                    }) super.emacs
-               else emacs25;
+               then super.emacs
+               # If the emacs package version is less than 25, use emacs25
+               else if compareVersions super.emacs.version "25" == -1
+                       then emacs25
+                       # Otherwise use the (newer) emacs package
+                       else super.emacs;
 
   # GTK crashes if X restarts, plus GTK3 is horrible and it's slow
   lucid = version.override { withGTK2 = false; withGTK3 = false; };
