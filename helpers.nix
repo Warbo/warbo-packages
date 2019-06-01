b57a14b57db86b80e01d4f95a2be165fc074458d
Chris Warburton <chriswarbo@gmail.com>
2019-06-01 12:46:07 +0100

    Bump helpers for pathcShebang

--- helpers.nix
+++ helpers.nix
@@ -2,7 +2,7 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "24590bf";
-    sha256 = "0yd5lzbdb5md3cvijn02bhdfb0ql3nr9a69d11nrz1bpzxw8473n";
+    rev    = "92a6b8c";
+    sha256 = "0vhbm4dag222wwvsgvl6lm67sn2yan80c1f1j90228wd1j0izc76";
   };
 }
