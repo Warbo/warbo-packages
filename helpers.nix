5b6623a0f46aba5d1207ecb24045317bb06028ef
Chris Warburton <chriswarbo@gmail.com>
2019-06-01 14:28:00 +0100

    Bump for shebang fix

--- helpers.nix
+++ helpers.nix
@@ -2,7 +2,7 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "92a6b8c";
-    sha256 = "0vhbm4dag222wwvsgvl6lm67sn2yan80c1f1j90228wd1j0izc76";
+    rev    = "110936c";
+    sha256 = "1iiwsszw651hykf21j4183w0d8nx4jxk0p5db3hpgxzhnihi50jg";
   };
 }
