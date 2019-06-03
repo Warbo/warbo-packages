dd48700e4d1049b5241d5d241e686d47fc5f2f7f
Chris Warburton <chriswarbo@gmail.com>
2019-06-03 20:07:05 +0100

    Don't bother setting no packages for Emacs

--- packages/emacsWithPkgs.nix
+++ packages/emacsWithPkgs.nix
@@ -1,27 +1,12 @@
-{ emacs25 ? null, emacsPackagesNgGen, hasBinary, super }:
+  # GTK crashes if X restarts, plus GTK3 is horrible and it's slow. This version
+  # forces the "lucid" GUI (AKA Xaw3D)
+{ emacs, hasBinary, lib }:
 
-with builtins;
-with rec {
-  # We want the latest Emacs version, but it gets a little messy:
-  # If there is no emacs25 package, use the emacs package
-  version = if emacs25 == null
-               then super.emacs
-               # If the emacs package version is less than 25, use emacs25
-               else if compareVersions super.emacs.version "25" == -1
-                       then emacs25
-                       # Otherwise use the (newer) emacs package
-                       else super.emacs;
-
-  # GTK crashes if X restarts, plus GTK3 is horrible and it's slow
-  lucid = version.override { withGTK2 = false; withGTK3 = false; };
-
-  pkg = (emacsPackagesNgGen lucid).emacsWithPackages (epkgs:
-    with epkgs;
-    with elpaPackages;
-    with melpaPackages;
-    []);
-};
-{
-  inherit pkg;
+rec {
+  pkg   = lib.makeOverridable ({ emacs }: emacs.override {
+                                withGTK2 = false;
+                                withGTK3 = false;
+                              })
+                              { inherit emacs; };
   tests = hasBinary pkg "emacs";
 }
