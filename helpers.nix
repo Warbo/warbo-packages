b5b7cb1c9a9c1a5c921fe8e956cf9e23b3425f47
Chris Warburton <chriswarbo@gmail.com>
2019-06-03 20:27:50 +0100

    Bump helpers for withNix sandbox fix

--- helpers.nix
+++ helpers.nix
@@ -2,7 +2,7 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "6d1ee67";
-    sha256 = "0y5260r2rrxr2b10501i5bkwx4zpf4p6xsz1dhpk5l8spn89c84w";
+    rev    = "25ddfc2";
+    sha256 = "0y6mj3zbiyx1z351sgpid17yw2b1672am1l2slhvd1z0aai86z6s";
   };
 }
