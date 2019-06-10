1f239cb08924ecd2fb07656e806e97352c059aed
Chris Warburton <chriswarbo@gmail.com>
2019-06-10 16:42:03 +0100

    Bamp

--- helpers.nix
+++ helpers.nix
@@ -2,7 +2,7 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "027e227";
-    sha256 = "11niics6rq60zaicb6spkfpvp8nv3wszdfgpqnrml946p1bggy13";
+    rev    = "1360d7c";
+    sha256 = "08xx2mxp7grdb318py2fb5nd8g9j602r4pxj1lgjqzbj4xz0r1g2";
   };
 }
