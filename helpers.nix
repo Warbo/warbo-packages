a46a1df7677821217b7ccc74548e1edbe35f0573
Chris Warburton <chriswarbo@gmail.com>
2020-01-17 09:51:06 +0000

    Bump helpers

--- helpers.nix
+++ helpers.nix
@@ -2,7 +2,7 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "25a03d3";
-    sha256 = "1sjsjp9slaf3zdjb7w0689si5bzm5ln8bihi16g36n89q4l4z3rd";
+    rev    = "9e032bf";
+    sha256 = "0p0vz1rnx955d03c3y2yydarjd8m96grn4bpdx7rlahqhcnjdk2v";
   };
 }
