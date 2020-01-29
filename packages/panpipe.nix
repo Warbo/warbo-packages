5e76f627d018fc035c1cccf74c0c036e74aee565
Chris Warburton <chriswarbo@gmail.com>
2020-01-29 02:27:57 +0000

    Re-enable panhandle tests

--- packages/panpipe.nix
+++ packages/panpipe.nix
@@ -11,13 +11,5 @@ with rec {
 };
 {
   pkg   = pkgs.panpipe.components.exes.panpipe;
-  tests = builtins.trace ''
-    FIXME: Disabling panpipe tests due to Nix error:
-      while evaluating the attribute 'configurePhase' of the derivation 'panhandle-0.4.0.0-test-tests' at /nix/store/7ck26hd855prl2xvf213y5ia88rnbcf5-nixpkgs1909/builder/comp-builder.nix:142:3:
-      while evaluating the attribute 'buildCommand' of the derivation 'panhandle-0.4.0.0-test-tests-config' at /nix/store/lj152b1wmfafjgxf6aqr5x75dqmfiw0y-nixpkgs1909/pkgs/build-support/trivial-builders.nix:7:14:
-      while evaluating 'concatMapStringsSep' at /nix/store/lj152b1wmfafjgxf6aqr5x75dqmfiw0y-nixpkgs1909/lib/strings.nix:88:5, called from /nix/store/7ck26hd855prl2xvf213y5ia88rnbcf5-nixpkgs1909/builder/make-config-files.nix:74:7:
-      while evaluating anonymous function at /nix/store/7ck26hd855prl2xvf213y5ia88rnbcf5-nixpkgs1909/builder/make-config-files.nix:74:37, called from undefined position:
-      attribute 'configFiles' missing, at /nix/store/7ck26hd855prl2xvf213y5ia88rnbcf5-nixpkgs1909/builder/make-config-files.nix:75:16
-  '';
-  #pkgs.panpipe.components.tests;
+  tests = pkgs.panpipe.components.tests;
 }
