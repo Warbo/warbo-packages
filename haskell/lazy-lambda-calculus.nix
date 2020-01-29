a8ba202a22feefbf2ecea5258808ca4b5ad38ebd
Chris Warburton <chriswarbo@gmail.com>
2020-01-29 00:51:36 +0000

    Bump lazy-lambda-calculus

--- haskell/lazy-lambda-calculus.nix
+++ haskell/lazy-lambda-calculus.nix
@@ -3,8 +3,8 @@ self: super: helf: huper:
 helf.callPackage (self.haskellGit {
   url    = "${self.repoSource or self.defaultRepo}/lazy-lambda-calculus.git";
   stable = {
-    rev        = "6674552";
-    sha256     = "1ill96m7ixy2yxdy1445nvdy6jrlg8fjwil4q07hiivprdj5xyh3";
+    rev        = "d4dfba7";
+    sha256     = "068cjb9wkqshyrjfg099zc8yp5s4q1ihnbby6hcs4d4r6z8fmchi";
     unsafeSkip = false;
   };
 }) {}
