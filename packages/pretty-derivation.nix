ad3eaaeb0906e1d3a732bce3a093a807f6b9a7c4
Chris Warburton <chriswarbo@gmail.com>
2020-02-03 23:30:56 +0000

    Cache pretty-derivation build plan

--- packages/pretty-derivation.nix
+++ packages/pretty-derivation.nix
@@ -9,6 +9,10 @@ with rec {
     version     = "1.0.2";
     ghc         = pkgs.buildPackages.pkgs.haskell-nix.compiler.ghc865;
     index-state = "2020-01-11T00:00:00Z";
+
+    # Update these when changing the derivation
+    plan-sha256  = "08lzvl34crpxnrqfk4af7khij1g28g5kdjmm8w7vmyk8wxqwgmqn";
+    materialized = ../caches/nix-derivation-plan-to-nix-pkgs;
   }) components;
 };
 {
