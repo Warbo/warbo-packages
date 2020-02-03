47506fcb5bff36623b2f78ec37d74c7f7c6845e9
Chris Warburton <chriswarbo@gmail.com>
2020-02-03 23:26:47 +0000

    Check that haskell-nix build plans are cached
    Running cabal to calculate a build plan can take several seconds, and this is
    done once per Haskell project. We cache the results to avoid needing to
    recalculate them every time, and check to see whether haskell-nix occurrences
    are accompanied by a reference to the cache. It's also important to update the
    cache whenever changes are made.

--- check.sh
+++ check.sh
@@ -3,10 +3,72 @@ set -e
 
 # Simple, quick sanity check. Useful as a git pre-commit hook.
 
+function fail {
+    echo "$*" 1>&2
+    exit 1
+}
+
 find . -name "*.nix" -type f | while read -r F
 do
     echo "Checking syntax of '$F'" 1>&2
     nix-instantiate --parse "$F" > /dev/null
+done
+
+echo "Checking that haskell-nix derivations are cached" 1>&2
+grep -R -l 'haskell-nix' | grep '\.nix$' | while read -r F
+do
+    grep 'plan-sha256' < "$F" > /dev/null || {
+        echo "File '$F' uses haskell-nix without a caching a plan-sha256" 1>&2
+        fail "Build the package and follow the instructions in 'trace'"
+    }
+    grep 'materialized' < "$F" > /dev/null || {
+        echo "File '$F' uses haskell-nix without a materialised plan"   1>&2
+        fail "Build the package and follow the instructions in 'trace'"
+    }
+
+    GOT=$(grep -o '\.\./caches/[^;]*' < "$F" | head -n1 | sed -e 's@\.\./@@g') ||
+        fail "Couldn't find any '../caches' reference in haskell-nix file '$F'"
+    D="$GOT/.plan.nix"
+    unset GOT
+
+    [[ -d "$D" ]] || fail "Couldn't find cache directory '$D' for '$F'"
+    COUNT=$(find "$D" -type f -name '*.nix' | wc -l)
+    [[ "$COUNT" -gt 0 ]] || fail "No .nix files in '$D'"
+    if [[ "$COUNT" -eq 1 ]]
+    then
+        X=$(readlink -f "$D"/*.nix)
+    else
+        # There are multiple files which may contain the main definition we're
+        # using. Try finding one whose name also appears in $F.
+        FOUND=0
+        for POSSIBLE in "$D"/*.nix
+        do
+            N=$(basename "$POSSIBLE" .nix)
+            if grep "\"$N\"" < "$F" > /dev/null
+            then
+                [[ "$FOUND" -eq 0 ]] ||
+                    fail "Ambiguity: Multiple files in '$D' are found in '$F'"
+                X="$POSSIBLE"
+            fi
+        done
+        unset FOUND
+        unset POSSIBLE
+    fi
+
+       FOUNDNAME=$(grep 'identifier' < "$X"      |
+                   grep -o 'name *= *"[^"]*"'    |
+                   grep -o '"[^"]*"'             ) || fail "No name in '$X'"
+    FOUNDVERSION=$(grep 'identifier' < "$X"      |
+                   grep -o 'version *= *"[^"]*"' |
+                   grep -o '"[^"]*"'             ) || fail "No version '$X'"
+    unset D
+
+    grep -F "$FOUNDNAME" < "$F" > /dev/null ||
+        fail "Expected name '$FOUNDNAME' in '$F', not found"
+    grep -F "$FOUNDVERSION" < "$F" > /dev/null ||
+        fail "Expected version '$FOUNDVERSION' in '$F', not found"
+    unset FOUNDNAME
+    unset FOUNDVERSION
 done
 
 # Allow failure to get HEAD (e.g. in case we're offline)
