246aa561cbd343b8b3e8b1b706947ecca8f7652b
Chris Warburton <chriswarbo@gmail.com>
2020-02-03 23:31:46 +0000

    Cache nix-diff build plan

--- packages/nix-diff.nix
+++ packages/nix-diff.nix
@@ -8,6 +8,10 @@ with rec {
     version     = "1.0.7";
     ghc         = pkgs.buildPackages.pkgs.haskell-nix.compiler.ghc865;
     index-state = "2020-01-11T00:00:00Z";
+
+    # Update these when the changing this derivation
+    plan-sha256  = "1j1wb27qbwjhx52n7brnyjsc66nhvfwblfyvvglah1rqwh580pvc";
+    materialized = ../caches/nix-diff-plan-to-nix-pkgs;
   }) components;
 };
 {
