53c38e484e8fe6c0e02831e080a60385e0b363e4
Chris Warburton <chriswarbo@gmail.com>
2020-02-03 17:06:07 +0000

    Override nix-diff

--- packages/nix-diff.nix
+++ packages/nix-diff.nix
@@ -1,6 +1,16 @@
-{ nixpkgs1803, super }:
+{ haskell-nix }:
 
+with rec {
+  pkgs = haskell-nix {};
+
+  inherit (pkgs.haskell-nix.hackage-package {
+    name        = "nix-diff";
+    version     = "1.0.7";
+    ghc         = pkgs.buildPackages.pkgs.haskell-nix.compiler.ghc865;
+    index-state = "2020-01-11T00:00:00Z";
+  }) components;
+};
 {
-  pkg   = super.nix-diff or nixpkgs1803.nix-diff;
-  tests = {};
+  inherit (components) tests;
+  pkg = components.exes.nix-diff;
 }
