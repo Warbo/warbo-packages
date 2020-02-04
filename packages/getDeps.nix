a5404a5bfb921b545d9b7ccb770466745fac3069
Chris Warburton <chriswarbo@gmail.com>
2020-02-04 14:22:34 +0000

    Use haskell-nix for GetDeps

--- packages/getDeps.nix
+++ packages/getDeps.nix
@@ -1,10 +1,23 @@
-{ haskellOverride, nixpkgs1803 }:
+{ defaultRepo, fetchgit, haskell-nix, repoSource ? defaultRepo }:
 
+with rec {
+  pkgs = haskell-nix {};
+
+  inherit ((pkgs.haskell-nix.cabalProject {
+    src = fetchgit {
+      url    = "${repoSource}/get-deps.git";
+      rev    = "cd6c171";
+      sha256 = "0lxgvbk58dq4hvlh3ld5m29apf2k06ihrkv6is0q4g5fc48k88cz";
+    };
+    ghc         = pkgs.buildPackages.pkgs.haskell-nix.compiler.ghc865;
+    index-state = "2020-01-11T00:00:00Z";
+
+    # Update these two when the derivation changes
+    plan-sha256  = "09d9lja0lzkgscwqm849ixsq4n35lnpa5f6m1krkrqz2r85lgk5s";
+    materialized = ../caches/GetDeps-plan-to-nix-pkgs;
+  }).GetDeps) components;
+};
 {
-  pkg = (haskellOverride {
-    haskellPackages = nixpkgs1803.haskell.packages.ghc7103;
-    filepath        = true;
-  }).getDeps;
-
-  tests = {};
+  inherit (components) tests;
+  pkg = components.exes.GetDeps;
 }
