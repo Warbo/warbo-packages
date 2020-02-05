d1cd5e82dd74ecf05d84d5c44e77a94b76499841
Chris Warburton <chriswarbo@gmail.com>
2020-02-05 08:46:59 +0000

    Disable gstreamer when building pidgin-privacy-please
    Its dependencies are broken on nixpkgs 19.09

--- packages/pidgin-privacy-please.nix
+++ packages/pidgin-privacy-please.nix
@@ -1,6 +1,9 @@
-{ autoconf, automake, fetchFromGitHub, glib, intltool, isBroken, libtool,
-  pidgin, pkgconfig, stdenv, unfix, unpack', useLocal ? true }:
+{ autoconf, automake, dummyBuild, fetchFromGitHub, glib, intltool, isBroken,
+  lib, libtool, overrideGstreamer ? true, pidgin, pkgconfig, stdenv, unfix,
+  unpack', useLocal ? true }:
 
+with builtins;
+with lib;
 with rec {
   local = unpack' "pidgin-privacy-please"
                   ../util/pidgin-privacy-please_0.7.1.orig.tar.gz;
@@ -18,11 +21,29 @@ with rec {
              preConfigure = "./autogen.sh";
              src          = upstream;
            };
+
+  # v4l-util is broken on Nixpkgs 19.09, but we don't need it for this build
+  pidginWithoutGstreamer = (pidgin.override (old: {
+    farstream = dummyBuild "farstream-dummy";
+    gst_all_1 = {
+      gst-plugins-base = dummyBuild "gst-plugins-base-dummy" // {
+        dev = dummyBuild "gst-plugins-good-dev-dummy";
+      };
+      gst-plugins-good = dummyBuild "gst-plugins-good-dummy";
+      gstreamer        = dummyBuild "gstreamer";
+    };
+  })).overrideAttrs (old: {
+    configureFlags = (old.configureFlags or []) ++ [
+      "--disable-gstreamer"
+      "--disable-vv"
+    ];
+  });
 };
 {
   pkg = stdenv.mkDerivation (env // {
-    name         = "pidgin-privacy-please";
-    buildInputs  = [ autoconf automake glib intltool libtool pidgin pkgconfig ];
+    name        = "pidgin-privacy-please";
+    buildInputs = [ autoconf automake glib intltool libtool pkgconfig ] ++
+      (if overrideGstreamer then [ pidginWithoutGstreamer ] else [ pidgin ]);
     installPhase = ''
       mkdir -p "$out/lib/pidgin"
       pushd src
