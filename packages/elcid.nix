70651424bac8dc337466b37de7a6b970c9905e8a
Chris Warburton <chriswarbo@gmail.com>
2020-02-05 15:26:07 +0000

    Force HOME when using 'go get'

--- packages/elcid.nix
+++ packages/elcid.nix
@@ -1,33 +1,32 @@
 { buildEnv, cacert, go, git, runCommand }:
 
 with rec {
+  goGet = name: pre: runCommand "go-get"
+    {
+      __noChroot     = true;
+      buildInputs    = [ go git ];
+      GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    }
+    ''
+      ${pre}
+      GOPATH="$PWD" HOME="$PWD" go get ${name}
 
-goGet = name: pre: runCommand "go-get"
-  {
-    __noChroot     = true;
-    buildInputs    = [ go git ];
-    GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-  }
-  ''
-    ${pre}
-    GOPATH="$PWD" go get ${name}
+      mkdir -p "$out"
+      cp -r ./bin "$out"/
+    '';
 
-    mkdir -p "$out"
-    cp -r ./bin "$out"/
+  elcid = goGet "github.com/whyrusleeping/elcid" ''
+    GOPATH="$PWD" go get github.com/whyrusleeping/elcid || true
+    pushd src/github.com/whyrusleeping/elcid
+      git checkout "9bcd412e289dae0e2d7b7039a1be33c5bc24ab38"
+    popd
   '';
 
-e = goGet "github.com/whyrusleeping/elcid" ''
-  GOPATH="$PWD" go get github.com/whyrusleeping/elcid || true
-  pushd src/github.com/whyrusleeping/elcid
-    git checkout "9bcd412e289dae0e2d7b7039a1be33c5bc24ab38"
-  popd
-'';
-
-b = goGet "github.com/whyrusleeping/bases" "";
+  bases = goGet "github.com/whyrusleeping/bases" "";
 
 };
 
 {
-  pkg   = buildEnv { name = "elcid"; paths = [ e b ]; };
+  pkg   = buildEnv { name = "elcid"; paths = [ bases elcid ]; };
   tests = {};
 }
