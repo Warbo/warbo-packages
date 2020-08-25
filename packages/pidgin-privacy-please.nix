230734d5edc3b837b90251b3fd5aed80af980514
Chris Warburton <chriswarbo@gmail.com>
2020-08-25 16:58:51 +0100

    Always use our copy of pidgin-privacy-please
    Upstream is gone

--- packages/pidgin-privacy-please.nix
+++ packages/pidgin-privacy-please.nix
@@ -1,26 +1,7 @@
-{ autoconf, automake, dummyBuild, fetchFromGitHub, glib, intltool, isBroken,
-  lib, libtool, pidgin, pkgconfig, stdenv, unfix, unpack' }:
+{ autoconf, automake, dummyBuild, getSource, glib, intltool, isBroken, lib,
+  libtool, pidgin, pkgconfig, stdenv, unfix, unpack' }:
 
-with builtins;
-with lib;
 with rec {
-  local = unpack' "pidgin-privacy-please"
-                  ../util/pidgin-privacy-please_0.7.1.orig.tar.gz;
-
-  upstream = fetchFromGitHub {
-    owner  = "cockroach";
-    repo   = "pidgin-privacy-please";
-    rev    = "8c63bcf";
-    sha256 = "1v175x73zhv0xmc202i10kvm0h1cpy55n94wja9dk77g05vhy84y";
-  };
-
-  env = useLocal: if useLocal
-                     then { src = local; }
-                     else {
-                       preConfigure = "./autogen.sh";
-                       src          = upstream;
-                     };
-
   # Rename to avoid shadowing
   pidginPristine = pidgin;
 
@@ -46,9 +27,10 @@ with rec {
     , pidgin            ? if overrideGstreamer
                              then pidginWithoutGstreamer
                              else pidginPristine
-    , useLocal          ? true
-    }: stdenv.mkDerivation (env useLocal // {
-      name        = "pidgin-privacy-please";
+    }: stdenv.mkDerivation {
+      name = "pidgin-privacy-please";
+      src  = unpack' "pidgin-privacy-please"
+                     ../util/pidgin-privacy-please_0.7.1.orig.tar.gz;
       buildInputs = [ autoconf automake glib intltool libtool pkgconfig ] ++
         (if overrideGstreamer then [ pidginWithoutGstreamer ] else [ pidgin ]);
       installPhase = ''
@@ -58,13 +40,9 @@ with rec {
                           "$out/lib/pidgin"
         popd
       '';
-    });
+    };
 };
 {
   pkg   = lib.makeOverridable makePlugin {};
-  tests = {
-    # Check that upstream is still missing (otherwise we'd prefer to use it).
-    # We need 'unfix' to strip the hashes, since 'isBroken' changes them.
-    localStillNeeded = unfix (isBroken upstream);
-  };
+  tests = {};
 }
