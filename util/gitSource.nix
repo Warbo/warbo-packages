24f91e86aa7fa2e9220e32518a40095e421c8cd7
Chris Warburton <chriswarbo@gmail.com>
2020-09-03 11:21:54 +0100

    git override fixes

--- util/gitSource.nix
+++ util/gitSource.nix
@@ -1,6 +1,6 @@
 # Pull the given name from niv's sources.nix, unless an alternative repo has
 # been configures, using the name 'repoSource'
-{ die, fetchgit, repoSource ? null, warbo-packages-sources }:
+{ die, repoSource ? null, warbo-packages-sources }:
 
 { name }:
   with rec {
@@ -13,8 +13,8 @@
     error = "gitSource only works for git/github sources (try getSource?)";
   };
   if repoSource == null
-     then source.outPath
-     else fetchgit {
-       inherit (source) rev sha256;
+     then source
+     else builtins.fetchgit {
+       inherit (source) rev;
        url = repoSource + "/" + baseNameOf source.repo;
      }
