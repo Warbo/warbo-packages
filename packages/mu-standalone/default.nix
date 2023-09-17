dc606d53e4534b0faaa8f79e159c5a2071aacb55
Chris Warburton <chriswarbo@gmail.com>
2023-09-17 16:56:37 +0100

    Delete mu-standalone (Nixpkgs mu package has changed too much)
    In particular, Nixpkgs's mu doesn't build with guile anymore, which is one of
    the things we were disabling. We also disabled its Emacs Lisp compilation, but
    that's no longer optional in Nixpkgs's mu.
    
    We could remove it by patching-out the postInstall, but I don't see much point.

--- packages/mu-standalone/default.nix
+++
@@ -1,5 +0,0 @@
-# Upstream mu requires Emacs to build mu4e and Guile to allow scripting. If we
-# just want the command line tools, we can do without those heavy dependencies.
-{ mu }:
-
-mu.override { emacs = null; guile = null; }
