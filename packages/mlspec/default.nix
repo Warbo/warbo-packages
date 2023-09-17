d80dd46169c51a4adff64c3ef800cee94d40d015
Chris Warburton <chriswarbo@gmail.com>
2023-09-17 17:00:42 +0100

    Skip MLSpec on ARM, since GHC from nixpkgs1803 isn't supported

--- packages/mlspec/default.nix
+++ packages/mlspec/default.nix
@@ -1,7 +1,7 @@
-{ warboHaskellOverride, nixEvalOverrides, nixpkgs1803, skipMac }:
+{ warboHaskellOverride, nixEvalOverrides, nixpkgs1803, skipARM }:
 
-skipMac "mlspec" (warboHaskellOverride {
+skipARM "mlspec" (warboHaskellOverride {
   haskellPackages = nixpkgs1803.haskell.packages.ghc7103;
-  filepath        = true;
-  extra           = nixEvalOverrides;
+  filepath = true;
+  extra = nixEvalOverrides;
 }).mlspec
