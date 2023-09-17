a71fdc2c2aabaf4641ae0b4d7b5302147e4d241c
Chris Warburton <chriswarbo@gmail.com>
2023-09-17 17:37:25 +0100

    Use nixpkgs-lib in check.nix

--- check.nix
+++ check.nix
@@ -1,19 +1,21 @@
 with rec {
   inherit (builtins) getAttr toJSON trace;
-  inherit (nix-helpers) gitHead nixpkgsLatest onlineCheck;
-  nix-helpers = import sources.nix-helpers.outPath;
-  sources     = import ./nix/sources.nix;
-  check       = name:
-    with { source = getAttr name sources; };
-    { got = source.rev; latest = gitHead { url = source.repo; }; };
+  inherit (nix-helpers) gitHead nixpkgs-lib onlineCheck;
+  inherit (import ./. { }) nix-helpers;
+  sources = import ./nix/sources.nix;
+  check = name:
+    with { source = getAttr name sources; }; {
+      got = source.rev;
+      latest = gitHead { url = source.repo; };
+    };
 };
-nixpkgsLatest.lib.foldl' (result: name:
-                           with check name;
-                           (if onlineCheck
-                               then got == latest || trace (toJSON {
-                                 inherit got latest name;
-                                 warning = "Source repo out of date";
-                               }) false
-                               else trace "Offline; skipping ${name} check" true) && result)
-                           true
-                           [ "nix-helpers" ]
+nixpkgs-lib.foldl' (result: name:
+  with { inherit (check name) got latest; };
+  (if onlineCheck then
+    got == latest || trace (toJSON {
+      inherit got latest name;
+      warning = "Source repo out of date";
+    }) false
+  else
+    trace "Offline; skipping ${name} check" true) && result) true
+[ "nix-helpers" ]
