7e4e0e9c9df049df6a631f6a928761e18d7f6c88
Chris Warburton <chriswarbo@gmail.com>
2023-09-26 13:00:26 +0100

    Bump nix-helpers for newer nixpkgs-lib

--- packages/nix-helpers/default.nix
+++ packages/nix-helpers/default.nix
@@ -1,5 +1,10 @@
 # TODO: Propagate nixpkgs-lib and nixpkgs, to allow overriding. We'll need to
 # bootstrap them, to prevent infinite recursion!
-{ warbo-packages-sources ? (import ../warbo-packages-sources { }) }:
+{ nix-helpers-args ? { } }:
 
-import warbo-packages-sources.nix-helpers { }
+import (builtins.fetchGit {
+  name = "nix-helpers";
+  ref = "master";
+  rev = "9bed246bfc3694e42e50f77b487fcb978a7eaaeb";
+  url = "http://chriswarbo.net/git/nix-helpers.git";
+}) nix-helpers-args
