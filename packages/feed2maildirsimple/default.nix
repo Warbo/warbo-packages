7a4a142494ce56ffb769b02c430d635b02f6f4f4
Chris Warburton <chriswarbo@gmail.com>
2023-09-30 17:08:59 +0100

    feed2maildirsimple: bump for python 3

--- packages/feed2maildirsimple/default.nix
+++ packages/feed2maildirsimple/default.nix
@@ -1,3 +1,8 @@
-{ gitSource, pkgs }:
+{ feed2maildirsimple-args ? { } }:
 
-import "${gitSource { name = "feed2maildir"; }}" { inherit pkgs; }
+import (fetchGit {
+  name = "feed2maildirsimple";
+  url = "http://chriswarbo.net/git/feed2maildir.git";
+  ref = "master";
+  rev = "0e452fad641119b58390813288687ffc5401463f";
+}) feed2maildirsimple-args
