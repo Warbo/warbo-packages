391ef10d23c87a3deb51dfac886414f5030295a6
Chris Warburton <chriswarbo@gmail.com>
2023-09-30 03:23:24 +0100

    artemis: use python3

--- packages/artemis/default.nix
+++ packages/artemis/default.nix
@@ -1,23 +1,27 @@
-{ darwin, getSource, pythonPackages, stdenv }:
+# Take pythonPackages from Nixpkgs 20.03 to avoid end-of-life errors in newer
+# releases.
+{ darwin, getSource, nixpkgs2003, stdenv }:
 
-with {
+with rec {
+  inherit (nixpkgs2003) pythonPackages;
+
   mercurial = pythonPackages.buildPythonPackage rec {
     name = "mercurial";
-    src  = getSource { inherit name; };
+    src = getSource { inherit name; };
 
     # Make sure ApplicationServices/ApplicationServices.h is available
-    propagatedBuildInputs =
-      if stdenv.isDarwin
-         then (with darwin.apple_sdk.frameworks; [
-           CoreGraphics
-           ApplicationServices
-           Carbon
-         ])
-         else [];
+    propagatedBuildInputs = if stdenv.isDarwin then
+      (with darwin.apple_sdk.frameworks; [
+        CoreGraphics
+        ApplicationServices
+        Carbon
+      ])
+    else
+      [ ];
   };
 };
 pythonPackages.buildPythonPackage rec {
-  name                  = "artemis";
-  src                   = getSource { inherit name; };
+  name = "artemis";
+  src = getSource { inherit name; };
   propagatedBuildInputs = [ mercurial ];
 }
