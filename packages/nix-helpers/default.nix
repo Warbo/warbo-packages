8a318cb1cbfc5dd8bc8c07e8abb018f89d02db44
Chris Warburton <chriswarbo@gmail.com>
2023-10-12 21:53:38 +0100

    nix-helpers: bump for suffixedFilesIn

--- packages/nix-helpers/default.nix
+++ packages/nix-helpers/default.nix
@@ -5,6 +5,6 @@
 import (builtins.fetchGit {
   name = "nix-helpers";
   ref = "master";
-  rev = "d222d6d28ca8f2d098bb507f99408f1e2e7c744e";
+  rev = "b5bca8d45026c826202d728b376effc672172c21";
   url = "http://chriswarbo.net/git/nix-helpers.git";
 }) nix-helpers-args
