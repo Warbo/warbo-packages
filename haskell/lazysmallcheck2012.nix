1f8eaa3001e1ab98a1daf751c4f1edcab642c9a0
Chris Warburton <chriswarbo@gmail.com>
2024-01-19 11:34:55 +0000

    Move and update lazysmallcheck2012

--- haskell/lazysmallcheck2012.nix
+++
@@ -1,6 +0,0 @@
-self: super: helf: huper:
-
-helf.callPackage (self.gitHaskell { name = "lazysmallcheck2012"; }) {
-  mkDerivation = args:
-    helf.mkDerivation (removeAttrs args [ "benchmarkHaskellDepends" ]);
-}
---
+++ packages/lazysmallcheck2012/default.nix
@@ -0,0 +1,4 @@
+import (fetchGit {
+  url = "http://chriswarbo.net/git/lazysmallcheck2012.git";
+  rev = "36ee2ce3a81e762bef5a5d57d8fe67c8e31bc574";
+})
