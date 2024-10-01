fb802ff3e4209b810a75f240af287319782c32f7
Chris Warburton <chriswarbo@gmail.com>
2024-10-01 18:10:03 +0100

    skulpture: simplify default.nix

--- packages/skulpture/default.nix
+++ packages/skulpture/default.nix
@@ -1,26 +1,5 @@
+{ qt5, qt6 }:
 {
-  cmake,
-  extra-cmake-modules,
-  libsForQt5,
-  qt5,
-  qt6,
-  stdenv,
-}:
-{
-  qt5 = import ./qt5.nix {
-    inherit
-      cmake
-      extra-cmake-modules
-      libsForQt5
-      qt5
-      ;
-  };
-  qt6 = import ./qt6.nix {
-    inherit
-      cmake
-      extra-cmake-modules
-      qt6
-      stdenv
-      ;
-  };
+  qt5 = qt5.callPackage ./qt5.nix { };
+  qt6 = qt6.callPackage ./qt6.nix { };
 }
