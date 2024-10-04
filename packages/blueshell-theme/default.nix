73882b03ba27de4029056f5cd7b5b9db8d778056
Chris Warburton <chriswarbo@gmail.com>
2024-10-04 18:22:28 +0100

    blueshell-theme: move source inline

--- packages/blueshell-theme/default.nix
+++ packages/blueshell-theme/default.nix
@@ -1,20 +1,22 @@
 {
   buildEnv,
-  getSource,
   gtk-engine-bluecurve,
   runCommand,
 }:
 with rec {
   name = "Blueshell";
-  src = runCommand name { src = getSource { inherit name; }; } ''
-    mkdir -p "$out/share/themes"
-    cp -r "$src" "$out/share/themes/Blueshell"
-  '';
+  src =  builtins.fetchGit {
+    url = "https://gitlab.com/KlipKyle/Blueshell.git";
+    rev = "d1700b7b043e81edd8d9294dd48e3116b8413252";
+  };
 };
 buildEnv {
   name = "blueshell-theme";
   paths = [
     gtk-engine-bluecurve
-    src
+    (runCommand name { inherit src; } ''
+      mkdir -p "$out/share/themes"
+      cp -r "$src" "$out/share/themes/Blueshell"
+    '')
   ];
 }
