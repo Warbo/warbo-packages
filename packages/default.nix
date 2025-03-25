2ea2df8a43e71498f58a71d2a6700b4438141a1c
Chris Warburton <chriswarbo@gmail.com>
2025-03-25 23:38:33 +0000

    Alter bootstrap
    Fetch fetchGitIPFS.nix from IPFS:
    
     - Allow custom gateway, using IPFS_GATEWAY env var. This is useful for getting
       data from a gateway we control, which we know has the data, like a local Kubo
       at http://localhost:8080
     - Work around a bug I noticed (and reported) in fetchTree
     - If nixpkgs was given, use that when instantiating fetchGitIPFS.nix
    
    Fetch nix-helpers using fetchGitIPFS.nix:
    
     - If we've been given a nix-helpers arg, use that instead of fetching anything
     - Allow overriding the nix-helpers tree ID that we fetch
     - Import the result, passing in nixpkgs and nixpkgs-lib if overridden
    
    Take nixpkgs and nixpkgs-lib from nix-helpers, if they're not overridden.
    
    The nix-helpers used during "bootstrapping" will be available in the 'newScope'
    we use to call our packages, at a higher precedence than our packages. Our
    nix-helpers package can hence ask for that as a nix-helpers argument and return
    it. Nice and simple!

--- packages/default.nix
+++ packages/default.nix
@@ -1,30 +1,80 @@
-{
-  nix-helpers ? import ./nix-helpers { },
-  nixpkgs ? nix-helpers.nixpkgs,
-  nixpkgs-lib ? nix-helpers.nixpkgs-lib,
+{ nix-helpers ? null
+, nix-helpers-tree ? { sha1 = "433f580482ca3b2fcae0aa45bc5c79cb59a539d7"; }
+, nixpkgs ? null
+, nixpkgs-lib ? null
 }:
-
 with rec {
-  inherit (builtins) removeAttrs;
-  inherit (nixpkgs-lib) mapAttrs;
-  inherit (nix-helpers) nixDirsIn nixFilesIn;
+  inherit (builtins)
+    currentSystem derivation fetchTree getEnv pathExists removeAttrs;
+  inherit (resolved.nixpkgs-lib) mapAttrs;
+  inherit (resolved.nix-helpers) nixDirsIn nixFilesIn;
+
+  fetchGitIPFS =
+    with rec {
+      # The version of fetchGitIPFS.nix. Shouldn't need updating often.
+      cid = "bafkreihec2oflgbudu5ncpttdyqsjcc74hs4k26b6absfdmqopqogajhj4";
+      narHash = "sha256-CgpPlgMjIt3DYVTJYmpdm/V5626f2s5lrEMtGEYQzTU=";
+
+      # fetchTree only takes one URL, so allow it to be overridden by env var.
+      override = getEnv "IPFS_GATEWAY";
+      gateway = if override == "" then "https://ipfs.io" else override;
+
+      # Workaround for https://github.com/NixOS/nix/issues/12751
+      # A derivation which copies 'src'. Since it's fixed-output, the resulting
+      # 'outPath' is independent of 'src' (it only depends on 'narHash').
+      fixed = src: derivation {
+        name = "source";
+        builder = "/bin/sh";
+        system = currentSystem;
+        outputHashMode = "nar";
+        outputHash = narHash;
+        args = [
+          "-c"
+          ''read -r -d "" content < ${src}; printf '%s\n' "$content" > "$out"''
+        ];
+      };
+      # See if we already have an outPath for this narHash, by checking with a
+      # dummy src: if so, use that path; otherwise call 'fetchTree'.
+      existing = (fixed "/dev/null").outPath;
+      file = if pathExists existing then existing else fixed (fetchTree {
+        inherit narHash;
+        type = "file";
+        url = "${gateway}/ipfs/${cid}";
+      });
+
+      raw = import file;
+    };
+    if nixpkgs == null then raw else (raw { pkgs = nixpkgs; }).fetchGitIPFS;
+
+  resolved = {
+    nixpkgs = if nixpkgs == null then resolved.nix-helpers.nixpkgs else nixpkgs;
+    nixpkgs-lib = if nixpkgs-lib == null
+                  then resolved.nix-helpers.nixpkgs-lib
+                  else nixpkgs-lib;
+    nix-helpers =
+      if nix-helpers == null
+      then import
+        (fetchGitIPFS nix-helpers-tree)
+        ((if nixpkgs == null then {} else { inherit nixpkgs; }) //
+         (if nixpkgs-lib == null then {} else { inherit nixpkgs-lib; }))
+      else nix-helpers;
+  };
 
   util = mapAttrs call (nixFilesIn ../util);
 
   # Like callPackage, but allows args to come from extraArgs
-  call = _: f: nixpkgs.newScope extraArgs f { };
+  call = _: f: resolved.nixpkgs.newScope extraArgs f { };
 
   # Include warbo-packages, so packages can reference each other. Put it first,
   # so that later attrsets will be checked first (making circular references
   # less likely)
   extraArgs =
     warbo-packages
-    // nix-helpers
+    // resolved.nix-helpers
     // util
-    // {
-      # Useful for overriding things
-      inherit extraArgs nix-helpers nixpkgs;
-    };
+    // resolved
+    # Useful for overriding things
+    // { inherit extraArgs; };
 
   load =
     filename:
--- packages/nix-helpers/default.nix
+++ packages/nix-helpers/default.nix
@@ -1,12 +1,3 @@
-# TODO: Propagate nixpkgs-lib and nixpkgs, to allow overriding. We'll need to
-# bootstrap them, to prevent infinite recursion!
-{
-  nix-helpers-args ? { },
-}:
-
-import (builtins.fetchGit {
-  name = "nix-helpers";
-  ref = "master";
-  rev = "120a170a3a92ecf08b49d712cf1209c74509d1db";
-  url = "http://chriswarbo.net/git/nix-helpers.git";
-}) nix-helpers-args
+# Take nix-helpers from extraArgs in ../../packages/default.nix
+{ nix-helpers }:
+nix-helpers
