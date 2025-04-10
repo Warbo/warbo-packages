5be292c3577f44e445419f5a8989c09aca629ada
Chris Warburton <chriswarbo@gmail.com>
2025-04-10 18:55:26 +0100

    git-on-ipfs: add new package

---
+++ packages/git-on-ipfs/default.nix
@@ -0,0 +1,8 @@
+{ fetchGitIPFS
+, git-on-ipfs-tree ? { sha1 = "9a062066f80299e7f42b6948ad716e5f64c1440c"; }
+, pkgs
+}:
+import (fetchGitIPFS git-on-ipfs-tree) {
+  inherit pkgs;
+  nixpkgs = pkgs.path;
+}
