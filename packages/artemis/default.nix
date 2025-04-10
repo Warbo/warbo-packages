a2a312f3c77ecca438a0f4010872013abfd685e6
Chris Warburton <chriswarbo@gmail.com>
2025-04-10 18:55:12 +0100

    artemis: fetch from IPFS

--- packages/artemis/default.nix
+++ packages/artemis/default.nix
@@ -1,8 +1,3 @@
-import (
-  builtins.fetchGit {
-    name = "artemis-src";
-    ref = "master";
-    rev = "c1d7730f3d26bf352dec3471d7b8a17dd443b3e0";
-    url = "http://chriswarbo.net/git/artemis.git";
-  }
-)
+import (import ../../util/fetchGitIPFS.nix {
+  sha1 = "f9a3c0d420f800575a141bf526f9b555ff6348c3";
+})
