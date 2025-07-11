c8af700e06af2af8428bcabb02a9c24984ccbbf7
Chris Warburton <chriswarbo@gmail.com>
2025-07-11 23:53:46 +0100

    nixfmt

--- packages/dhtkv/default.nix
+++ packages/dhtkv/default.nix
@@ -5,16 +5,17 @@ buildNpmPackage rec {
   version = "2921e2a64939905d522e4ff44230025ba5769c76";
   dontNpmBuild = true;
   npmDepsHash = "sha256-G9nMckJ/KxutKyJyjItCGYLicM/ri62ec85GWDEm6pA=";
-  src = runCommand "dhtkv"
-    {
-      raw = builtins.fetchGit {
-        url = "https://github.com/max-mapper/dhtkv.git";
-        rev = version;
-      };
-    }
-    ''
-      cp -r "$raw" "$out"
-      chmod -R +w "$out"
-      cp ${./package-lock.json} "$out/package-lock.json"
-    '';
+  src =
+    runCommand "dhtkv"
+      {
+        raw = builtins.fetchGit {
+          url = "https://github.com/max-mapper/dhtkv.git";
+          rev = version;
+        };
+      }
+      ''
+        cp -r "$raw" "$out"
+        chmod -R +w "$out"
+        cp ${./package-lock.json} "$out/package-lock.json"
+      '';
 }
--- packages/mldht/default.nix
+++ packages/mldht/default.nix
@@ -1,4 +1,8 @@
-{ jdk, mkMavenPackage, fetchTreeFromGitHub }:
+{
+  jdk,
+  mkMavenPackage,
+  fetchTreeFromGitHub,
+}:
 mkMavenPackage rec {
   pname = "mldht";
   version = "0.1.1-SNAPSHOT";
@@ -14,16 +18,16 @@ mkMavenPackage rec {
     repo = "mldht";
     tree = "3cdc9ae1153e77910e701e43bb8279fee8951672";
   };
-  installSteps = [
-    ''cp -r bin "$out"''
-    ''sed -i -e "s@java@${jdk}/bin/java@g" "$out/bin/mldht-daemon"''
-    ''sed -i -e 's@& ) &$@ )@g' "$out/bin/mldht-daemon"''
-    ''mkdir "$out/target"''
-    ''cp -r target/dependency "$out/target/"''
-    ''cp ./target/libmldht*.jar "$out/target/"''
-  ] ++ map
-    (pattern: ''sed -i -e "s/${pattern}//g" "$out/bin/mldht-daemon"'')
+  installSteps =
     [
+      ''cp -r bin "$out"''
+      ''sed -i -e "s@java@${jdk}/bin/java@g" "$out/bin/mldht-daemon"''
+      ''sed -i -e 's@& ) &$@ )@g' "$out/bin/mldht-daemon"''
+      ''mkdir "$out/target"''
+      ''cp -r target/dependency "$out/target/"''
+      ''cp ./target/libmldht*.jar "$out/target/"''
+    ]
+    ++ map (pattern: ''sed -i -e "s/${pattern}//g" "$out/bin/mldht-daemon"'') [
       # Remove some old JVM options that no longer exist
       ''-XX:.UseConcMarkSweepGC''
       ''-XX:.UseParNewGC''
