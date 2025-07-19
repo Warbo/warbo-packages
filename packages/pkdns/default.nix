c20b35807514181d3191dfc3074d52ffa39e86b3
Chris Warburton <chriswarbo@gmail.com>
2025-07-19 10:58:22 +0100

    pkdns: work around for quoting issues
    Raised upstreams as https://github.com/pubky/pkarr/issues/164 but I don't know
    if my fix might break other things.

--- packages/pkdns/default.nix
+++ packages/pkdns/default.nix
@@ -2,6 +2,7 @@
   lib,
   fetchTreeFromGitHub,
   rustPlatform,
+  runCommand,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "pkdns-server";
@@ -12,6 +13,17 @@ rustPlatform.buildRustPackage rec {
     tree = "2d3e533e8616716606fd227b74aa9cdd535e3dfb";
   };
   cargoLock.lockFile = "${src}/Cargo.lock";
+  cargoPatches = [
+    (runCommand "pkarr-local.patch" { base = ./pkarr-path.patch; } ''
+      sed -e 's@PKARR_PATH@${
+        fetchGit {
+          url = "https://github.com/Warbo/pkarr.git";
+          ref = "txt-quotes";
+          rev = "743a048da2cf3026d92439005a621f7d22b658c8";
+        }
+      }/pkarr@g' < "$base" > "$out"
+    '')
+  ];
   doCheck = false; # Tests attempt to use network
   meta = with lib; {
     description = "DNS server resolving pkarr self-sovereign domains";
