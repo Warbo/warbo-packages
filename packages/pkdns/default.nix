a879e2ec2692d22fc313f3410d80f9c4ce758ca0
Chris Warburton <chriswarbo@gmail.com>
2025-07-21 19:09:30 +0100

    pkdns: upstream is fixed now

--- packages/pkdns/default.nix
+++ packages/pkdns/default.nix
@@ -8,22 +8,11 @@ rustPlatform.buildRustPackage rec {
   pname = "pkdns-server";
   version = "0.7.0-rc.3";
   src = fetchTreeFromGitHub {
-    owner = "Warbo";
+    owner = "pubky";
     repo = "pkdns";
-    tree = "23901cacfdd80ebe97ee6234a79593fae7f3bdcf";
+    tree = "7e261baf04fc48c4773adff87455f5cbb27a7ab6";
   };
   cargoLock.lockFile = "${src}/Cargo.lock";
-  cargoPatches = [
-    (runCommand "pkarr-local.patch" { base = ./pkarr-path.patch; } ''
-      sed -e 's@PKARR_PATH@${
-        fetchGit {
-          url = "https://github.com/Warbo/pkarr.git";
-          ref = "txt-quotes";
-          rev = "743a048da2cf3026d92439005a621f7d22b658c8";
-        }
-      }/pkarr@g' < "$base" > "$out"
-    '')
-  ];
   doCheck = false; # Tests attempt to use network
   meta = with lib; {
     description = "DNS server resolving pkarr self-sovereign domains";
