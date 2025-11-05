bc909c0fff49e80951cb91a4b9d3c76fae9e20aa
Chris Warburton <chriswarbo@gmail.com>
2025-11-05 01:56:17 +0000

    git-on-ipfs

--- packages/git-on-ipfs/default.nix
+++ packages/git-on-ipfs/default.nix
@@ -4,8 +4,9 @@
     sha1 = "9a062066f80299e7f42b6948ad716e5f64c1440c";
   },
   pkgs,
+  warbo-packages,
 }:
 import (fetchGitIPFS git-on-ipfs-tree) {
-  inherit pkgs;
+  inherit pkgs warbo-packages;
   nixpkgs = pkgs.path;
 }
