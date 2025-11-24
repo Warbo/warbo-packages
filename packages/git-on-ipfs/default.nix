4609b855fbb39155e7061de045171171fd94df5c
Chris Warburton <chriswarbo@gmail.com>
2025-11-24 23:53:43 +0000

    git-on-ipfs: Use newScope to allow overrides
    Specifically, the '.override' function for each thing in packages/ will be for
    the function defined by its default.nix file; NOT for the return value of such a
    call. Hence if we do e.g. '{ callPackage }: callPackage foo {}' then we get a
    '.override' for replacing that '{ callPackage }' argument set; not for the 'foo'
    function.
    
    To work around this, we use ', ... }@args' to accept arbitrary extra
    arguments (including whatever our git-on-ipfs function might want), and we use
    'newScope args git-on-ipfs {}' so that the given 'args' will override the attrs
    used to select argument values for git-on-ipfs. It's a little indirect, and
    won't give us error messages if we misspell an argument name, but it's a simple
    way to allow overriding.

--- packages/git-on-ipfs/default.nix
+++ packages/git-on-ipfs/default.nix
@@ -3,10 +3,9 @@
   git-on-ipfs-tree ? {
     sha1 = "3c09a6a28891d1f28ba8a210cbba36a7304042c3";
   },
+  newScope,
+  nixpkgs ? pkgs.path,
   pkgs,
-  warbo-packages,
-}:
-import (fetchGitIPFS git-on-ipfs-tree) {
-  inherit pkgs warbo-packages;
-  nixpkgs = pkgs.path;
-}
+  ...
+}@args:
+newScope args (fetchGitIPFS git-on-ipfs-tree) {}
