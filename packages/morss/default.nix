66c29db4520a1850f3cf71a1e42715608aedd7b8
Chris Warburton <chriswarbo@gmail.com>
2025-12-03 09:51:56 +0000

    morss: Fix to work with Nixpkgs 25.11 buildPythonPackage

--- packages/morss/default.nix
+++ packages/morss/default.nix
@@ -5,6 +5,9 @@
 python3Packages.buildPythonApplication rec {
   pname = "morss";
   version = "20230627.2039";
+  pyproject = true;
+  build-system = [ python3Packages.setuptools ];
+  dontCheckRuntimeDeps = true; # Avoids "bs4 not installed"
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-xAGsKSHYHx0vjsMu8QfFdO/7S4mQXELVGjBqaSiMfDw=";
@@ -12,12 +15,15 @@ python3Packages.buildPythonApplication rec {
   propagatedBuildInputs = with python3Packages; [
     beautifulsoup4
     chardet
-    dateutil
+    python-dateutil
     lxml
   ];
   passthru.lib = python3Packages.buildPythonPackage {
     inherit
       pname
+      pyproject
+      build-system
+      dontCheckRuntimeDeps
       version
       src
       propagatedBuildInputs
