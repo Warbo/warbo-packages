3c90e40b58d30d3c389cffb83bebbbccdf4ee85f
Chris Warburton <chriswarbo@gmail.com>
2025-12-04 16:30:51 +0000

    Bump nix-helpers

--- packages/default.nix
+++ packages/default.nix
@@ -3,7 +3,7 @@
   nix-helpers ? null,
   nix-helpers-tree ? {
     # git-ref: pkipfs::yhqn1j1d7i5xb7456639i4uw1e5deeuddjsnyrow46zuxipwgijo master^{tree}
-    sha1 = "19264ec494c40c1c6c00945a872b48dee4791692";
+    sha1 = "36e6f014409a55d08f5e67757ae50049c7e51f1b";
   },
   nixpkgs ? null,
   nixpkgs-lib ? null,
