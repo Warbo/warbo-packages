9b653c407e6855ec462d540c1bd99d32633acad9
Chris Warburton <chriswarbo@gmail.com>
2026-01-14 10:44:51 +0000

    Sort lines

--- packages/megadrive/default.nix
+++ packages/megadrive/default.nix
@@ -1,17 +1,17 @@
 { megadrive-launcher }:
 builtins.mapAttrs (name: sha256: megadrive-launcher { inherit name sha256; }) {
-  EarthwormJim = "sha256-xFLTMGofLwQPawTOh2xs0UyxxdBy/0G6eRu9N5f+kRM=";
-  SonicTheHedgehog = "sha256-RhYLqgY2LHEcnxpQF8tzcQJkRJNsivXpOniZbPMv8qY=";
   Animaniacs = "sha256-Mfv51ELVeMQ3iKAkM2Y5ZI1cfatFAmNuEnGQyZH/NgQ=";
-  StreetsOfRage2 = "sha256-SjFO2/7pIoKFD+lcTHZJIZFu/Z08Inf97CWBJ5sTabE=";
-  Sonic2AndKnuckles = "sha256-ZD8BJ/Gi2kmMK8kFopFzQokmDMT8JuzaQWwDvJZxAt0=";
-  SonicAndKnuckles = "sha256-5bIvJ7ZlJrUdzYoH5B3XFfMVbZX4PUmrYlvlHzZK4PI=";
+  EarthwormJim = "sha256-xFLTMGofLwQPawTOh2xs0UyxxdBy/0G6eRu9N5f+kRM=";
   EarthwormJim2 = "sha256-dVdrSq1GqzzwyMvN6DJWWaoWWG+BHCJ5T3t4w1BQM2Y=";
-  ToeJamAndEarl = "sha256-K9+4m8yEg89pKFwbynr6ekHNpZ022g8Gjo9gqSMEMig=";
   Ristar = "sha256-zdQZ8Dh1izT07A+NvwUK5GnSsFOm1fjwRsyOetwE794=";
-  SonicTheHedgehog3 = "sha256-Dm5BwHa6FmMHQYLCDq/DPF8A9lWgtC1GvRz9E0zZHyo=";
+  Sonic2AndKnuckles = "sha256-ZD8BJ/Gi2kmMK8kFopFzQokmDMT8JuzaQWwDvJZxAt0=";
   Sonic3AndKnuckles = "sha256-M0WbTDzAqx0vNGEqHD6M+rV1Dm45TYbqSsS3UFC/5Lg=";
-  SonicTheHedgehog2 = "sha256-FdJ+E/br1YRz198fc6HvherB9y86jVk4Yxa9284G9M0=";
-  ZeroWing = "sha256-iny7ZiUe3xfWl0HMgQU1uMOYO1GIbUMW1YVqG5T1F4Q=";
   Sonic3D = "sha256-LeKK1uSwhPtB5Ka6Khn866Z16hVMdwYwsNX/6kzjGqA=";
+  SonicAndKnuckles = "sha256-5bIvJ7ZlJrUdzYoH5B3XFfMVbZX4PUmrYlvlHzZK4PI=";
+  SonicTheHedgehog = "sha256-RhYLqgY2LHEcnxpQF8tzcQJkRJNsivXpOniZbPMv8qY=";
+  SonicTheHedgehog2 = "sha256-FdJ+E/br1YRz198fc6HvherB9y86jVk4Yxa9284G9M0=";
+  SonicTheHedgehog3 = "sha256-Dm5BwHa6FmMHQYLCDq/DPF8A9lWgtC1GvRz9E0zZHyo=";
+  StreetsOfRage2 = "sha256-SjFO2/7pIoKFD+lcTHZJIZFu/Z08Inf97CWBJ5sTabE=";
+  ToeJamAndEarl = "sha256-K9+4m8yEg89pKFwbynr6ekHNpZ022g8Gjo9gqSMEMig=";
+  ZeroWing = "sha256-iny7ZiUe3xfWl0HMgQU1uMOYO1GIbUMW1YVqG5T1F4Q=";
 }
