138764cc426564d9719534e5f29a802f26ab9bf4
Chris Warburton <chriswarbo@gmail.com>
2026-01-20 19:09:50 +0000

    ps1: More games

--- packages/ps1/default.nix
+++ packages/ps1/default.nix
@@ -2,4 +2,6 @@
 builtins.mapAttrs (name: discs: ps1-launcher { inherit name discs; }) {
   "Crash Bandicoot"."disc.bin" =
     "mEiA11Lg3EIrcTFMTpFwPIDqna8CRGHMgTpCFEhhzeeuY7A";
+  "Oddworld Abes Oddysee"."disc.bin" =
+    "mEiAYvzIIgJgQEadVAmNSrceB0IEOTjznbMBPUcqYnTgDQA";
 }
