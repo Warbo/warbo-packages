daa2a4652340f02b10567ed0ff60a9c63664007a
Chris Warburton <chriswarbo@gmail.com>
2026-01-30 01:14:05 +0000

    TODO

--- packages/feed2maildirsimple/default.nix
+++ packages/feed2maildirsimple/default.nix
@@ -1,3 +1,5 @@
+# TODO: Create a pkarr URL, push latest version to it, then update this (and
+# annotate for update-git-refs)
 { fetchGitIPFS, nixpkgs }:
 import (fetchGitIPFS { sha1 = "99ef713450d5f32b8eb6a5964ed3430906e9a776"; }) {
   inherit nixpkgs;
