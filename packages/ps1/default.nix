1c317554953d1ea83b254274c46dcb8f92c7cd7f
Chris Warburton <chriswarbo@gmail.com>
2026-01-30 20:04:47 +0000

    ps1: more entries

--- packages/ps1/default.nix
+++ packages/ps1/default.nix
@@ -1,7 +1,225 @@
 { ps1-launcher }:
 builtins.mapAttrs (name: discs: ps1-launcher { inherit name discs; }) {
-  "Crash Bandicoot"."disc.bin" =
-    "mEiA11Lg3EIrcTFMTpFwPIDqna8CRGHMgTpCFEhhzeeuY7A";
-  "Oddworld Abes Oddysee"."disc.bin" =
-    "mEiAYvzIIgJgQEadVAmNSrceB0IEOTjznbMBPUcqYnTgDQA";
+  "Alone In The Dark The New Nightmare" = [
+    {
+      "Disc1.chd" =
+        "f12208edf8a2207c27f51dbbb3d4bfedbb9c40d7f5a0cee3c02e745d06264cfabd5b9";
+    }
+    {
+      "Disc 2.chd" =
+        "f12208e1a231fba9a41a84eacf5d7b017b7e0d189efe474ae49544b014b1f4fcf0612";
+    }
+  ];
+
+  "Bloody Roar 2"."BloodyRoarII.chd" =
+    "f1220dbfd58d0d3a0508297258e398b130ce53af223d9325390186b8411dbe6927893";
+
+  "Chaos Break"."ChaosBreak.chd" =
+    "f1220bb0a86764789e9cfeaa35c58373dd38c5dcc5792e2aa6598b0bbcda81290eaee";
+
+  "Clock Tower"."ClockTower.chd" =
+    "f1220e83a3d4c70ff735b3821fd136006ba5022f5f95c865e653107b2f043cfed31c9";
+
+  "Colin McRae Rally 2"."ColinMcRaeRally2.0Rev2.chd" =
+    "f122049749632a21eaade622a328be95fabd2969b19e52ed5518c9364ea04abe7b148";
+
+  "Crash Bandicoot"."CrashBandicoot.bin" =
+    "f122035d4b837108adc4c5313a45c0f203aa76bc0911873204e908512187379eb98ec";
+
+  "Crash Bash"."CrashBash.chd" =
+    "f1220e29074b394b8dc7184b03d95ff032cb26ec132e2ad329b558b98349037869e36";
+
+  "Crash Team Racing"."CrashTeamRacing.chd" =
+    "f12203ad2efbf964ef06274f5c00830a199d99cdf80902c7d9e979b9959a431153534";
+
+  "Dino Crisis"."DinoCrisis.chd" =
+    "f1220b7fa24909b085237f694bb4f6a0cd7efff572d12951df4859dee75c1d1e3dde4";
+
+  "Echo Night"."EchoNight.chd" =
+    "f12200dff94f89e02e63a2cbe68a3a4154121dc1349c988b1ae873823bbed2fba1942";
+
+  "Final Fantasy 7" = [
+    {
+      "FinalFantasyVIIDisc1.chd" =
+        "f1220657123d2beb8bc688104f132dc3efd66f0cf19978f0730d1e65ea42f6a7acb99";
+    }
+    {
+      "FinalFantasyVIIDisc2.chd" =
+        "f12204f37006bd68ede95c57aaa647bb2f415da72e6ce5a88974902516fa9ea59c852";
+    }
+    {
+      "FinalFantasyVIIDisc3.chd" =
+        "f122000413fc780bbf41e6c0baeff05dbaa9576114443c21ea95e2f92853abd525b47";
+    }
+  ];
+
+  "Final Fantasy 8" = [
+    {
+      "Disc1.chd" =
+        "f1220197138f04b1c91edfdce73a40256755e1390bcce00f36451633bc088c65ebd84";
+    }
+    {
+      "Disc2.chd" =
+        "f1220922169c5fbfe1b7142cc0a040284b46430bbd5119e905ea83b5c9361ccf04a80";
+    }
+    {
+      "Disc3.chd" =
+        "f1220e44398443be91e7cc0232928da08c414cb77a3335dc71e233bd63129d0aba18e";
+    }
+    {
+      "Disc4.chd" =
+        "f122055ac704886603ff5be546e2814193f2bab07528aabee4ade940a265e36968396";
+    }
+  ];
+
+  "Final Fantasy 9" = [
+    {
+      "Disc1.bin" =
+        "f12203add7528c87fecd87133e7af64a067f90d7b9d8ed446493464fefc5125049e0f";
+    }
+    {
+      "Disc2.bin" =
+        "f1220df44eb1e9f9e129055ce772329f630da0aab132158f15ee96de30ebe478bdb2e";
+    }
+    {
+      "Disc3.bin" =
+        "f122081841ee6a892b2eb4eae283a97137a069d540b86e5ebaf2e9342a3bb706e05c3";
+    }
+    {
+      "Disc4.bin" =
+        "f1220c6aad6b96ecd86d59ea7e933af6ae7b0b3defd93dc5978292f16a8771cf5b45d";
+    }
+  ];
+
+  Galerians = [
+    {
+      "Disc1.chd" =
+        "f12204b41f9abeadda1b328404b5d618a67367f3b357d053a6c333b013c5082e6708f";
+    }
+    {
+      "Disc2.chd" =
+        "f1220dbf4c76ef20beca472ea2f1b5104c7ad6b3a02f3854ac616b59821eb0cfd3bbd";
+    }
+    {
+      "Disc3.chd" =
+        "f12206a19c30c965bd829220c116d296eb1f0b7856d6c52f52a296b5a6dbe86a52dce";
+    }
+  ];
+
+  "Gex 3D Enter The Gecko"."EnterTheGecko.chd" =
+    "f12207fbdfa6efe06f792ba0d3e6a6e851e8a4122fdf4eac0ac8104fa84dacaef68e8";
+
+  "Intelligent Qube"."IQ.chd" =
+    "f1220085e88651a99b285411786fc01425b5fddb7cb56d3fd2864390135820d1cbc75";
+
+  Iru."Iru_v1.0.chd" =
+    "f12200e56aead9bbe896b0fef5ab8358e6b9653d4f7e9fb25d8cfceafe72865d32eef";
+
+  Koudelka = [
+    {
+      "Disc1.chd" =
+        "f12205dc076bb71aa7d5a14deba36e84b710f75bf4cf9791f22dd57877a8a8ba21f00";
+    }
+    {
+      "Disc2.chd" =
+        "f1220fdf8b526a59c3e9d2cf3317dc664e485c4464a09f535649dd993a1db29035d92";
+    }
+    {
+      "Disc3.chd" =
+        "f1220b0348f535dc53c5ba05cde9764e07d8fed80f1c3f4c7f7c674688cf920f150bd";
+    }
+    {
+      "Disc4.chd" =
+        "f1220646088f16d6d724a0976d2caf9d8c733d0b724dd5761e0427b1c3ffa8b7643a2";
+    }
+  ];
+
+  "Legacy Of Kain Blood Omen"."BloodOmenLegacyOfKain.chd" =
+    "f12209e40f898458c8a40e3fa05e381280c4667e42ff2d07ac969c89593764f8030fb";
+
+  "Legacy Of Kain Soul Reaver"."SoulReaver.chd" =
+    "f122053e6bfabf23711553568224fbca7a5f3e8f1494bb84e08ce3b2b7112249d504d";
+
+  "Metal Gear Solid" = [
+    {
+      "Disc1.chd" =
+        "f1220e637941fca92b05d47352a30fbc6b79e28952dbbf722ef11f6aa5d0d7a272c2f";
+    }
+    {
+      "Disc2.chd" =
+        "f1220695e1a0ac2656399320952386bfd6a0d96f0c40b0e4faed87f4405866f46779d";
+    }
+  ];
+
+  "Micro Machines 3"."MicroMachinesV3.bin" =
+    "f1220b165c844493a4ef10e491e24dfe195324e46c68b6b60d793e372476450e519cd";
+
+  "Micro Maniacs"."MicroManiacs.bin" =
+    "f1220ddfdbf86e1c7b001ff6a9188ce11725a9555678861d488e420ecaa27d0a62166";
+
+  "Nightmare Creatures"."NightmareCreatures.chd" =
+    "f12204d50e10f9926a882d99efd9690faad54269cb746c5c8f3631c689de03c891b8e";
+
+  "Nightmare Creatures 2"."NightmareCreaturesII.chd" =
+    "f1220428243f705c7adb6c8f68deedb5d2c57fccc06f49c330173233091913e8c2985";
+
+  "Oddworld Abes Oddysee"."AbesOddysee.bin" =
+    "f122018bf320880981011a755026352adc781d0810e4e3ce76cc04f51ca989d380340";
+
+  "Parasite Eve" = [
+    {
+      "Disc1.chd" =
+        "f12206d76aed5c106901532d3382a8030a1cf9567832b27b7b1701bfdcdb189703969";
+    }
+    {
+      "Disc2.chd" =
+        "f12206e460fb30c0606162002d89fb09acb6ffdae35e4d81331ed076a843fa4db73ff";
+    }
+  ];
+
+  "Point Blank"."PointBlank.bin" =
+    "f12207fc946de9e5fd0252f0424b8c959a3028fbfc906bf0daabac5826f9bc6ad2c10";
+
+  "Resident Evil 2 Claire" = [
+    {
+      "Claire.chd" =
+        "f1220e90efb2034070b7c4255e2ab4c6f821a2c69be4da0e8ed5081b3db11b720e074";
+    }
+    {
+      "Leon.chd" =
+        "f1220aeb8cc46bcd18acf9708c169be1be20994ff3917ba9ac98aff0dc68c177af56e";
+    }
+  ];
+
+  "Resident Evil 2 Leon" = [
+    {
+      "Leon.chd" =
+        "f1220aeb8cc46bcd18acf9708c169be1be20994ff3917ba9ac98aff0dc68c177af56e";
+    }
+    {
+      "Claire.chd" =
+        "f1220e90efb2034070b7c4255e2ab4c6f821a2c69be4da0e8ed5081b3db11b720e074";
+    }
+  ];
+
+  "Resident Evil 3"."ResidentEvil3Nemesis.bin" =
+    "f1220dc3f29d7f7046fbdc85e8c376721a8880567f0cda309a52ee5da875ae9e40157";
+
+  "Silent Hill"."SilentHill.chd" =
+    "f12206ded16c3a1f56b591975f3a9dbc810a4b2d2ad247b39d548cf28837930be0c51";
+
+  "Tekken 2"."Tekken2.chd" =
+    "f1220e9527bdb0fcefaf70784d4c8e35d072407e6fe435b954df016195b895095b6a3";
+
+  "Tekken 3"."Tekken3.bin" =
+    "f1220f20a200eaaf276986fe11f1ba2f1316eb2c0c46ae5da9870d47e9bb01baa1f90";
+
+  "Tomb Raider"."TombRaider_v1.4.chd" =
+    "f12200d8f3807699d23190701e09efe93ca7b08fa4fbc79421b9e125c51cbb25bdf62";
+
+  "Tony Hawks Pro Skater"."TonyHawksProSkater.chd" =
+    "f122050f09367028360480874d241c4c31a9daec4116389a1a3cde9d9665e00773cc8";
+
+  "Twisted Metal 2"."TwistedMetal2.chd" =
+    "f122053ec58e94b002bf99e43170cce93c86ceafcb043f1d9530041f32a469084b256";
 }
