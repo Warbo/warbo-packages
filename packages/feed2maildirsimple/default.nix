7f6a5a2d2802d93527953824a35516f257d83491
Chris Warburton <chriswarbo@gmail.com>
2026-03-26 21:19:45 +0000

    feed2maildirsimple: use pkgs as propagating argument

--- packages/feed2maildirsimple/default.nix
+++ packages/feed2maildirsimple/default.nix
@@ -1,6 +1,10 @@
 # TODO: Create a pkarr URL, push latest version to it, then update this (and
 # annotate for update-git-refs)
-{ fetchGitIPFS, nixpkgs }:
+{
+  fetchGitIPFS,
+  nixpkgs ? pkgs,
+  pkgs,
+}:
 import (fetchGitIPFS { sha1 = "b09784865958c98bbe88478b143d9a769a08cb0b"; }) {
   inherit nixpkgs;
 }
