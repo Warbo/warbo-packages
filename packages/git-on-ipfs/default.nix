8fffeda68d4bf613bc187dc1367edf4bdb6869df
Chris Warburton <chriswarbo@gmail.com>
2026-04-27 17:03:55 +0100

    git-on-ipfs: bumpy

--- packages/git-on-ipfs/default.nix
+++ packages/git-on-ipfs/default.nix
@@ -2,7 +2,7 @@
   fetchGitIPFS,
   git-on-ipfs-tree ? {
     # multiaddr: /ipns/master.y5a9inx61aski4miz4sgmg55qgbazxhfwab3i6ee1ypa6rnumi8o/tree
-    sha1 = "sha1-x4jIb5WXN2JP1HBxNxzSOzi+KII=";
+    sha1 = "sha1-sRXSX+9/9stFRi70b5Qx528CFx0=";
   },
   newScope,
   nixpkgs ? pkgs.path,
